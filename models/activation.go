1db9675f86a1b988497a65c04be42ff7a44162a8
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2014-10-23 16:33:14 +0200

    Add activation model

---
+++ models/activation.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"github.com/astaxie/beego/orm"
+	"time"
+)
+
+func init() {
+	orm.RegisterModel(new(Activation))
+}
+
+type Activation struct {
+	Id               int `orm:"auto";"pk"`
+	InvoiceId        int
+	UserId           int
+	MachineId        int
+	Active           bool
+	TimeStart        time.Time
+	TimeEnd          time.Time
+	TimeTotal        int
+	UsedKwh          float32
+	DiscountPercents float32
+	DiscountFixed    float32
+	VatRate          float32
+	CommentRef       string `orm:"size(255)"`
+	Invoiced         bool
+	Changed          bool
+}
