8bae42e1415a9cb10fce654b079bf7d75a7e4f62
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2014-12-11 12:05:15 +0100

    Add mysql config variables to app.conf

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
@@ -12,13 +13,22 @@ func init() {
 	beego.SetLogFuncCall(false)
 	// beego.SetLevel(beego.LevelInfo)
 
-	// Template init
+	// Template init, we replace the default template tags
+	// as AngularJS uses the same ones as GoLang
 	beego.TemplateLeft = "<<<"
 	beego.TemplateRight = ">>>"
 
-	// Database init
+	// Setup database
+	mysqlUser := beego.AppConfig.String("mysqluser")
+	mysqlPass := beego.AppConfig.String("mysqlpass")
+	mysqlHost := beego.AppConfig.String("mysqlhost")
+	mysqlDb := beego.AppConfig.String("mysqldb")
+
+	mysqlConnString := fmt.Sprintf("%s:%s@%s/%s?charset=utf8",
+		mysqlUser, mysqlPass, mysqlHost, mysqlDb)
+
 	orm.RegisterDriver("mysql", orm.DR_MySQL)
-	orm.RegisterDataBase("default", "mysql", "root:root@/fabsmith?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", mysqlConnString)
 }
 
 func main() {
