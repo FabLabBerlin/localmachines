109646544b0a769a04ab55ece3254e3793fdc9a5
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-01-05 14:21:29 +0100

    Polish db connection config interpretation in main.go

--- main.go
+++ main.go
@@ -9,24 +9,31 @@ import (
 )
 
 func init() {
-	// Debug init
-	beego.SetLogFuncCall(false)
-	// beego.SetLevel(beego.LevelInfo)
+	// Set debug level for our app
+	beego.SetLevel(beego.LevelInformational)
 
 	// Template init, we replace the default template tags
 	// as AngularJS uses the same ones as GoLang
 	beego.TemplateLeft = "<<<"
 	beego.TemplateRight = ">>>"
 
-	// Setup database
+	// Get MySQL database variables from config file
 	mysqlUser := beego.AppConfig.String("mysqluser")
 	mysqlPass := beego.AppConfig.String("mysqlpass")
 	mysqlHost := beego.AppConfig.String("mysqlhost")
+	mysqlPort := beego.AppConfig.String("mysqlport")
 	mysqlDb := beego.AppConfig.String("mysqldb")
 
-	mysqlConnString := fmt.Sprintf("%s:%s@%s/%s?charset=utf8",
-		mysqlUser, mysqlPass, mysqlHost, mysqlDb)
+	// If MySQL port is empty, replace with default value
+	if mysqlPort == "" {
+		mysqlPort = "3306"
+	}
 
+	// Build MySQL connection string out of the config variables
+	mysqlConnString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8",
+		mysqlUser, mysqlPass, mysqlHost, mysqlPort, mysqlDb)
+
+	// Register MySQL driver and default database for beego ORM
 	orm.RegisterDriver("mysql", orm.DR_MySQL)
 	orm.RegisterDataBase("default", "mysql", mysqlConnString)
 }
