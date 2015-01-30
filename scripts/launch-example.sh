94e04032da58e8e6183155628b5cd605c792cc05
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-01-30 15:10:26 +0100

    Update example launch script

--- scripts/launch-example.sh
+++ scripts/launch-example.sh
@@ -10,9 +10,4 @@
 #
 # 4. Use it!
 
-sudo FABSMITH_RUNMODE="prod" \
-FABSMITH_HTTP_PORT="80" \
-FABSMITH_MYSQL_USER="youruser" \
-FABSMITH_MYSQL_PASS="yourpass" \
-FABSMITH_MYSQL_DB="fabsmith" \
-/full/path/to/fabsmith
+sudo FABSMITH_RUNMODE="prod" /full/path/to/fabsmith
