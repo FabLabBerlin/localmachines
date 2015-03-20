4e9be2a64baa357d3a8ebfaae3626c4acb3191e3
Philip Silva <psilva.de@gmail.com>
2015-03-20 15:46:48 +0100

    Admin: User: showing all the data

--- clients/admin/dev/ng-modules/user/user.js
+++ clients/admin/dev/ng-modules/user/user.js
@@ -11,8 +11,65 @@ app.config(['$routeProvider', function($routeProvider) {
 }]); // app.config
 
 app.controller('UserCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
-	$scope.userId = $routeParams.userId;
-	console.log('$scope.userId: ', $scope.userId);
+	$scope.user = {
+		Id: $routeParams.userId
+	};
+	$scope.userMachines = [];
+	$scope.userRoles = {};
+
+	$http({
+		method: 'GET',
+		url: '/api/users/' + $scope.user.Id,
+		params: {
+			anticache: new Date().getTime()
+		}
+	})
+	.success(function(data) {
+		console.log('Got user');
+		console.log(data);
+		$scope.user = data;
+	})
+	.error(function(data, status) {
+		console.log('Could not get user');
+		console.log('Data: ' + data);
+		console.log('Status code: ' + status);
+	});
+
+	$http({
+		method: 'GET',
+		url: '/api/users/' + $scope.user.Id + '/machines',
+		params: {
+			anticache: new Date().getTime()
+		}
+	})
+	.success(function(data) {
+		console.log('Got user machines');
+		console.log(data);
+		$scope.userMachines = data;
+	})
+	.error(function(data, status) {
+		console.log('Could not get user machines');
+		console.log('Data: ' + data);
+		console.log('Status code: ' + status);
+	});
+
+	$http({
+		method: 'GET',
+		url: '/api/users/' + $scope.user.Id + '/roles',
+		params: {
+			anticache: new Date().getTime()
+		}
+	})
+	.success(function(data) {
+		console.log('Got user roles');
+		console.log(data);
+		$scope.userRoles = data;
+	})
+	.error(function(data, status) {
+		console.log('Could not get user roles');
+		console.log('Data: ' + data);
+		console.log('Status code: ' + status);
+	});
 }]); // app.controller
 
 })(); // closure
