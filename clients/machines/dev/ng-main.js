724245560c50b2ae1b1db02b3b96c1c69227fc44
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-03-25 13:47:08 +0100

    Convert tabs indentation to 4 spaces in machines client main

--- clients/machines/dev/ng-main.js
+++ clients/machines/dev/ng-main.js
@@ -22,112 +22,112 @@ var app = angular.module('fabsmith', [
 // This checks whether an user is logged in always before 
 // switching to a new view
 app.run(['$rootScope', '$location', '$http', 
-	function($rootScope, $location, $http) {
+    function($rootScope, $location, $http) {
 
-	// On each location change
-	$rootScope.$on('$locationChangeStart', function(event, newUrl, oldUrl) {
+    // On each location change
+    $rootScope.$on('$locationChangeStart', function(event, newUrl, oldUrl) {
 
-		// Get requested angular path
-		var newPath = newUrl.split('#')[1];
-		
-		// If it is not login (main) view, 
-		// check if the user is logged in
-		// TODO: figure out a way that does not need a request
-		if (newPath !== '/login') {
-			$http({
-				method: 'POST',
-				url: '/api/users/login',
-				params: {
-					username: 'blank', // TODO: randomize?
-					password: 'blank',
-					anticache: new Date().getTime()
-				}
-			})
-			.success(function(data) {
-				if (data.Status !== 'logged') {
-					event.preventDefault();
-					$location.path('/login');
-				} else {
-					if (newPath) {
-						$location.path(newPath);
-					} else {
-						$location.path('/machines');
-					}
-				}
-			})
-			.error(function() {
-				event.preventDefault();
-				$location.path('/login');
-			});
-		}
+        // Get requested angular path
+        var newPath = newUrl.split('#')[1];
+        
+        // If it is not login (main) view, 
+        // check if the user is logged in
+        // TODO: figure out a way that does not need a request
+        if (newPath !== '/login') {
+            $http({
+                method: 'POST',
+                url: '/api/users/login',
+                params: {
+                    username: 'blank', // TODO: randomize?
+                    password: 'blank',
+                    anticache: new Date().getTime()
+                }
+            })
+            .success(function(data) {
+                if (data.Status !== 'logged') {
+                    event.preventDefault();
+                    $location.path('/login');
+                } else {
+                    if (newPath) {
+                        $location.path(newPath);
+                    } else {
+                        $location.path('/machines');
+                    }
+                }
+            })
+            .error(function() {
+                event.preventDefault();
+                $location.path('/login');
+            });
+        }
 
-	});
+    });
 }]);
 
 // Configure http provider to send regular form POST data instead of JSON
 app.config(['$httpProvider', function($httpProvider) {
 
-	$httpProvider.defaults.transformRequest = function(data){
-		if (data === undefined) {
-		    return data;
-		}
-		return $.param(data);
-	};
-	$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+    $httpProvider.defaults.transformRequest = function(data){
+        if (data === undefined) {
+            return data;
+        }
+        return $.param(data);
+    };
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
 
 }]);
 
 // Main controller, checks if user logged in
 app.controller('MainCtrl', ['$scope', '$http', '$location', '$cookieStore', '$cookies', 
-	function($scope, $http, $location, $cookieStore, $cookies){
+    function($scope, $http, $location, $cookieStore, $cookies){
 
-	// Store user data on user login
-	$scope.putUserData = function(data) {
-		for (var key in data) {
-			if (data.hasOwnProperty(key)) {
-				$cookieStore.put(key, data[key]);
-			}
-		}
-	};
-	$scope.$on('user-login', function (event, data){
-		$scope.putUserData(data);
-	});
+    // Store user data on user login
+    $scope.putUserData = function(data) {
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+                $cookieStore.put(key, data[key]);
+            }
+        }
+    };
+    $scope.$on('user-login', function (event, data){
+        $scope.putUserData(data);
+    });
 
-	// Clear user data on user logout
-	$scope.removeUserData = function() {
-		for (var key in $cookies) {
-			if ($cookies.hasOwnProperty(key)) {
-				$cookieStore.remove(key);
-			}
-		}
-	};
+    // Clear user data on user logout
+    $scope.removeUserData = function() {
+        for (var key in $cookies) {
+            if ($cookies.hasOwnProperty(key)) {
+                $cookieStore.remove(key);
+            }
+        }
+    };
 
-	// Log out on logout event
-	$scope.logout = function() {
-		$scope.removeUserData();
+    // Log out on logout event
+    $scope.logout = function() {
+        $scope.removeUserData();
 
-		$http({
-			method: 'GET',
-			url: '/api/users/logout',
-			params: {
-				anticache: new Date().getTime()
-			}
-		})
-		.success(function() {
-			$location.path('/');
-		})
-		.error(function() {
-			toastr.error('Failed to log out. Probably server down.');
-			$location.path('/');
-		});
+        $http({
+            method: 'GET',
+            url: '/api/users/logout',
+            params: {
+                anticache: new Date().getTime()
+            }
+        })
+        .success(function() {
+            $location.path('/');
+        })
+        .error(function() {
+            toastr.error('Failed to log out. Probably server down.');
+            $location.path('/');
+        });
 
-	};
-	$scope.$on('timer-stopped', function (event, data){
-		$scope.logout();
-	});
-	$scope.$on('logout', function (event, data){
-		$scope.logout();
-	});
+    };
+    $scope.$on('timer-stopped', function (event, data){
+        $scope.logout();
+    });
+    $scope.$on('logout', function (event, data){
+        $scope.logout();
+    });
 
 }]);
 
