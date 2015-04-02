60584d8f0624bd268f3fb1eafceb3ad2fe559ca0
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-04-02 12:53:58 +0200

    Add add machine prompt to admin client machines module

--- clients/admin/dev/ng-modules/machines/machines.js
+++ clients/admin/dev/ng-modules/machines/machines.js
@@ -5,16 +5,30 @@
 var app = angular.module('fabsmith.admin.machines', ['ngRoute', 'ngCookies']);
 
 app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/machines', {
-        templateUrl: 'ng-modules/machines/machines.html',
-        controller: 'MachinesCtrl'
-    });
+  $routeProvider.when('/machines', {
+    templateUrl: 'ng-modules/machines/machines.html',
+    controller: 'MachinesCtrl'
+  });
 }]); // app.config
 
 app.controller('MachinesCtrl', ['$scope', '$http', '$location', 
  function($scope, $http, $location) {
 
- 	// Stuff goes here
+  $scope.addMachinePrompt = function() {
+    vex.dialog.prompt({
+      message: 'Enter machine name',
+      placeholder: 'Machine name',
+      callback: $scope.machinePromptCallback
+    });
+  };
+
+  $scope.machinePromptCallback = function(value) {
+    if (value) {
+      toastr.success('Creating machine');
+    } else if (value !== false) {
+      toastr.error('No machine name');
+    }
+  };
 
 }]); // app.controller
 
