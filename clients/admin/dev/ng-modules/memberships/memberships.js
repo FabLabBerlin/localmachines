0a403f75f2a9c3ead27006cb52c3cbb60409f698
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-04-08 11:14:45 +0200

    Add membership basics to admin client
     - Add dynamic table
     - Add membership prompt
     - Add placeholder scope functions

--- clients/admin/dev/ng-modules/memberships/memberships.js
+++ clients/admin/dev/ng-modules/memberships/memberships.js
@@ -5,16 +5,46 @@
 var app = angular.module('fabsmith.admin.memberships', ['ngRoute', 'ngCookies']);
 
 app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/memberships', {
-        templateUrl: 'ng-modules/memberships/memberships.html',
-        controller: 'MembershipsCtrl'
-    });
+  $routeProvider.when('/memberships', {
+    templateUrl: 'ng-modules/memberships/memberships.html',
+    controller: 'MembershipsCtrl'
+  });
 }]); // app.config
 
 app.controller('MembershipsCtrl', ['$scope', '$http', '$location', 
  function($scope, $http, $location) {
 
- 	// Stuff goes here
+  $scope.memberships = [
+    {
+      Id: 1,
+      Title: 'My Membership',
+      ShortName: 'MM',
+      Duration: 1,
+      Unit: 'day',
+      Price: 30,
+      MachinePriceDeduction: 50
+    }
+  ];
+
+  $scope.addMembershipPrompt = function() {
+    vex.dialog.prompt({
+      message: 'Enter membership name',
+      placeholder: 'Membership name',
+      callback: $scope.membershipPromptCallback
+    });
+  };
+
+  $scope.membershipPromptCallback = function(value) {
+    console.log(value);
+  };
+
+  $scope.addMembership = function() {
+    toastr.warning('adding membership');
+  };
+
+  $scope.editMembership = function() {
+    toastr.warning('editing membership');
+  };
 
 }]); // app.controller
 
