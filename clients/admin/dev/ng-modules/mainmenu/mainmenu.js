e3c4f8a3462ac1b11ac17431d039644a25bcdafc
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-04-13 13:41:06 +0200

    Remove main menu controller in admin client

--- clients/admin/dev/ng-modules/mainmenu/mainmenu.js
+++ clients/admin/dev/ng-modules/mainmenu/mainmenu.js
@@ -4,10 +4,6 @@
 
 var app = angular.module('fabsmith.admin.mainmenu', ['ngRoute', 'ngCookies']);
 
-app.controller('MainMenuCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
-  //
-}]); // app.controller
-
 app.directive('mainMenu', function() {
   return {
     templateUrl: 'ng-modules/mainmenu/mainmenu.html',
