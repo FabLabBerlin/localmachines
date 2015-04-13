bb097f41d2847ea4a26f454200937805da96f81b
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-04-13 13:34:29 +0200

    Simplified login check in admin client

--- clients/admin/dev/ng-main.js
+++ clients/admin/dev/ng-main.js
@@ -22,65 +22,56 @@ var app = angular.module('fabsmith', [
 ]);
 
 // This checks whether an user is logged in always before switching to a new view
-app.run(['$rootScope', '$location', '$http', function($rootScope, $location, $http) {
-	$rootScope.$on('$locationChangeStart', function(event, newUrl, oldUrl) {
-		
-		// Get requested angular path
-		var newPath = newUrl.split('#')[1];
-		
-		// If it is not login (main) view, 
-		// check if the user is logged in
-		// TODO: figure out a way that does not need a request
-		if (newPath !== '/login') {
-			$http({
-				method: 'POST',
-				url: '/api/users/login',
-				params: {
-					username: 'blank', // TODO: randomize?
-					password: 'blank',
-					anticache: new Date().getTime()
-				}
-			})
-			.success(function(data) {
-				if (data.Status !== 'logged') {
-					event.preventDefault();
-					$location.path('/login');
-				} else {
-					if (newPath) {
-						$location.path(newPath);
-					} else {
-						$location.path('/dashboard');
-					}
-				}
-			})
-			.error(function() {
-				event.preventDefault();
-				$location.path('/login');
-			});
-		}
+app.run(['$rootScope', '$location', '$http', '$cookieStore', 
+ function($rootScope, $location, $http, $cookieStore) {
+  
+  $rootScope.$on('$locationChangeStart', 
+   function(event, newUrl, oldUrl) {
 
-	});
+    // Get requested angular path
+    var newPath = newUrl.split('#')[1];
+    var userId = $cookieStore.get('Id');
+    
+    // Only check if user logged in if requested page is other
+    // than the login page
+    if (newPath !== '/login') {
+
+      // Just use the cookie user ID to check log in status.
+      // Very simple and no double requests.
+      if (userId) {
+        if (newPath) {
+          $location.path(newPath);
+        } else {
+          $location.path('/dashboard');
+        }
+      } else {
+        $location.path('/login');
+      }
+
+    }
+
+  });
 }]); // app.run
 
 // Configure http provider to send regular form POST data instead of JSON
 app.config(['$httpProvider', function($httpProvider) {
-	$httpProvider.defaults.transformRequest = function(data){
-		if (data === undefined) {
-		    return data;
-		}
-		return $.param(data);
-	};
-	$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+  $httpProvider.defaults.transformRequest = function(data){
+    if (data === undefined) {
+        return data;
+    }
+    return $.param(data);
+  };
+  $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
 
-	// Disable caching (http://stackoverflow.com/a/19771501/485185)
-	if (!$httpProvider.defaults.headers.get) {
-		$httpProvider.defaults.headers.get = {};
-	}
-	//disable IE ajax request caching
-	$httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
-	// extra
-	$httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
-	$httpProvider.defaults.headers.get.Pragma = 'no-cache';
+  // Disable caching (http://stackoverflow.com/a/19771501/485185)
+  if (!$httpProvider.defaults.headers.get) {
+    $httpProvider.defaults.headers.get = {};
+  }
+  //disable IE ajax request caching
+  $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+  // extra
+  $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+  $httpProvider.defaults.headers.get.Pragma = 'no-cache';
 
 }]); // app.config
 
@@ -88,56 +79,56 @@ app.config(['$httpProvider', function($httpProvider) {
 app.controller('MainCtrl', ['$scope', '$http', '$location', '$cookieStore', '$cookies', 
  function($scope, $http, $location, $cookieStore, $cookies){
 
- 	// Configure toastr default location
- 	toastr.options.positionClass = 'toast-bottom-left';
+  // Configure toastr default location
+  toastr.options.positionClass = 'toast-bottom-left';
 
-	// Configure vex theme
-	vex.defaultOptions.className = 'vex-theme-plain';
+  // Configure vex theme
+  vex.defaultOptions.className = 'vex-theme-plain';
 
-	// Store user data on user login
-	$scope.putUserData = function(data) {
-		for (var key in data) {
-			if (data.hasOwnProperty(key)) {
-				$cookieStore.put(key, data[key]);
-			}
-		}
-	};
-	$scope.$on('user-login', function (event, data){
-		$scope.putUserData(data);
-	});
+  // Store user data on user login
+  $scope.putUserData = function(data) {
+    for (var key in data) {
+      if (data.hasOwnProperty(key)) {
+        $cookieStore.put(key, data[key]);
+      }
+    }
+  };
+  $scope.$on('user-login', function (event, data){
+    $scope.putUserData(data);
+  });
 
-	// Clear user data on user logout
-	$scope.removeUserData = function() {
-		for (var key in $cookies) {
-			if ($cookies.hasOwnProperty(key)) {
-				$cookieStore.remove(key);
-			}
-		}
-	};
+  // Clear user data on user logout
+  $scope.removeUserData = function() {
+    for (var key in $cookies) {
+      if ($cookies.hasOwnProperty(key)) {
+        $cookieStore.remove(key);
+      }
+    }
+  };
 
-	// Log out on logout event
-	$scope.logout = function() {
-		$scope.removeUserData();
+  // Log out on logout event
+  $scope.logout = function() {
+    $scope.removeUserData();
 
-		$http({
-			method: 'GET',
-			url: '/api/users/logout',
-			params: {
-				anticache: new Date().getTime()
-			}
-		})
-		.success(function() {
-			$location.path('/');
-		})
-		.error(function() {
-			alert('Failed to log out. Probably server down.');
-			$location.path('/');
-		});
+    $http({
+      method: 'GET',
+      url: '/api/users/logout',
+      params: {
+        anticache: new Date().getTime()
+      }
+    })
+    .success(function() {
+      $location.path('/');
+    })
+    .error(function() {
+      alert('Failed to log out. Probably server down.');
+      $location.path('/');
+    });
 
-	};
-	$scope.$on('logout', function (event, data){
-		$scope.logout();
-	});
+  };
+  $scope.$on('logout', function (event, data){
+    $scope.logout();
+  });
 }]);
 
 })(); // closure
