36e1f07a388fa4a877868cd73eeb8034f58c56ea
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-04-13 13:38:35 +0200

    Update indentation in main menu module of the admin client

--- clients/admin/dev/ng-modules/mainmenu/mainmenu.js
+++ clients/admin/dev/ng-modules/mainmenu/mainmenu.js
@@ -5,26 +5,27 @@
 var app = angular.module('fabsmith.admin.mainmenu', ['ngRoute', 'ngCookies']);
 
 app.controller('MainMenuCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
-	//
+  //
 }]); // app.controller
 
 app.directive('mainMenu', function() {
-	return {
-		templateUrl: 'ng-modules/mainmenu/mainmenu.html',
-		restrict: 'E',
-		controller: ['$scope', '$element', function($scope, $element){
-			var links = $($element).find('a');
-			links.click(function(){
+  return {
+    templateUrl: 'ng-modules/mainmenu/mainmenu.html',
+    restrict: 'E',
+    controller: ['$scope', '$element', function($scope, $element) {
 
-				// The float CSS parameter is changed to 'none' whenever the
-				// window width is below Bootstrap grid breakpoint (768px)
-				var navfloat = $('.navbar-header').css('float');
-				if (navfloat === 'none') {
-					$($element).find('.navbar-collapse').collapse('hide');
-				}				
-			});
-		}]
-	};
+      var links = $($element).find('a');
+      links.click(function(){
+
+        // The float CSS parameter is changed to 'none' whenever the
+        // window width is below Bootstrap grid breakpoint (768px)
+        var navfloat = $('.navbar-header').css('float');
+        if (navfloat === 'none') {
+          $($element).find('.navbar-collapse').collapse('hide');
+        }       
+      });
+    }]
+  };
 });
 
 })(); // closure
