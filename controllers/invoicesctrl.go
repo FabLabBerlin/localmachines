e7539c8d42c7ddd29c0d03de7fb42e9a22c96c9d
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-04-15 20:15:01 +0200

    Add backend support for creating invoices (not fully working yet)
     - Can get sum of activations per machine per user
     - Saves a placeholder xlsx file and returns it's path via API

--- controllers/invoicesctrl.go
+++ controllers/invoicesctrl.go
@@ -2,7 +2,8 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
-	//"github.com/kr15h/fabsmith/models"
+	"github.com/kr15h/fabsmith/models"
+	"time"
 )
 
 type InvoicesController struct {
@@ -13,14 +14,69 @@ type InvoicesController struct {
 // @Description Create invoice from selection of activations
 // @Param	startDate		query 	string	true		"Period start date"
 // @Param	endDate		query 	string	true		"Period end date"
-// @Param	userId		query 	int	true		"User ID"
-// @Param	includeInvoiced		query 	bool	true		"Whether to include already invoiced activations"
 // @Success 200 {object} models.Invoice
 // @Failure	403	Failed to create invoice
 // @Failure	401	Not authorized
 // @router / [post]
 func (this *InvoicesController) Create() {
 
-	beego.Info("Invoices controller")
+	// Only admin can use this API call
+	if !this.IsAdmin() {
+		beego.Error("Not authorized")
+		this.CustomAbort(401, "Not authorized")
+	}
 
+	var err error
+	var startDate string
+	var endDate string
+
+	// Get variables
+	startDate = this.GetString("startDate")
+	if startDate == "" {
+		beego.Error("Missing start date")
+		this.CustomAbort(403, "Failed to create invoice")
+	}
+
+	endDate = this.GetString("endDate")
+	if endDate == "" {
+		beego.Error("Missing end date")
+		this.CustomAbort(403, "Failed to create invoice")
+	}
+
+	beego.Trace("startDate:", startDate)
+	beego.Trace("endDate:", endDate)
+
+	// Convert / parse string time values as time.Time
+	var timeForm = "2006-01-02"
+	var startTime time.Time
+
+	startTime, err = time.ParseInLocation(
+		timeForm, startDate, time.Now().Location())
+	if err != nil {
+		beego.Error("Failed to parse startDate:", err)
+		this.CustomAbort(403, "Failed to create invoice")
+	}
+
+	var endTime time.Time
+
+	endTime, err = time.ParseInLocation(
+		timeForm, endDate, time.Now().Location())
+	if err != nil {
+		beego.Error("Failed to parse endDate:", err)
+		this.CustomAbort(403, "Failed to create invoice")
+	}
+
+	beego.Trace(startTime)
+	beego.Trace(endTime)
+
+	// Create invoice
+	var invoice *models.Invoice
+	invoice, err = models.CreateInvoice(startTime, endTime)
+	if err != nil {
+		beego.Error("Failed to create invoice:", err)
+		this.CustomAbort(403, "Failed to create invoice")
+	}
+
+	this.Data["json"] = invoice
+	this.ServeJson()
 }
--- models/invoices.go
+++ models/invoices.go
@@ -1,15 +1,18 @@
 package models
 
 import (
+	"errors"
+	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
+	"github.com/tealeg/xlsx"
 	"time"
 )
 
 type Invoice struct {
 	Id          int64  `orm:"auto";"pk"`
 	Activations string `orm:type(text)`
-	XmlFile     string `orm:size(255)`
+	XlsFile     string `orm:size(255)`
 }
 
 func (this *Invoice) TableName() string {
@@ -21,23 +24,132 @@ func init() {
 }
 
 func CreateInvoice(startTime time.Time,
-	endTime time.Time,
-	userId int64,
-	includeInvoiced bool) (*Invoice, error) {
+	endTime time.Time) (*Invoice, error) {
 
 	beego.Info("Creating invoice...")
 
-	// Load all the matching activations
+	act := Activation{}
+	usr := User{}
+	mch := Machine{}
 
-	// Load activation users
+	o := orm.NewOrm()
 
-	// Load user memberships
+	// Get unique users
+	users := []User{}
+	query := fmt.Sprintf("SELECT DISTINCT u.* FROM %s u JOIN %s a ON a.user_id=u.id "+
+		"WHERE a.time_start>? AND a.time_end<? AND a.invoiced=false AND a.active=false ",
+		usr.TableName(),
+		act.TableName())
 
-	// Sort activations per user
+	num, err := o.Raw(query,
+		startTime.Format("2006-01-02"),
+		endTime.Format("2006-01-02")).QueryRows(&users)
 
-	// Sort user activations per machine
+	if err != nil {
+		msg := fmt.Sprintf("Failed to get unique users: %v", err)
+		return nil, errors.New(msg)
+	}
 
-	// Calculate total price per activation per machine
-	// By taking in account the user membership
-	return nil, nil
+	beego.Trace("Num uniq users:", num)
+
+	// Create a xlsx file if there
+	var file *xlsx.File
+	var sheet *xlsx.Sheet
+	var row *xlsx.Row
+	var cell *xlsx.Cell
+
+	if len(users) > 0 {
+
+		file = xlsx.NewFile()
+		sheet = file.AddSheet("Sheet1")
+		row = sheet.AddRow()
+		cell = row.AddCell()
+		cell.Value = "I am a cell!"
+
+	} else {
+		return nil, errors.New("There are no invoiceable activations")
+	}
+
+	// Loop through users
+	for _, user := range users {
+
+		// 1. Get unique user machines within the range of activations
+		machines := []Machine{}
+		query = fmt.Sprintf("SELECT DISTINCT m.* FROM %s m JOIN %s a ON a.machine_id=m.id "+
+			"WHERE a.time_start>? AND a.time_end<? AND a.invoiced=false "+
+			"AND a.active=false AND a.user_id=?",
+			mch.TableName(),
+			act.TableName())
+
+		num, err = o.Raw(query,
+			startTime.Format("2006-01-02"),
+			endTime.Format("2006-01-02"),
+			user.Id).QueryRows(&machines)
+
+		if err != nil {
+			msg := fmt.Sprintf("Failed to get user machines: %v", err)
+			return nil, errors.New(msg)
+		}
+
+		beego.Trace("Num uniq machines:", num)
+
+		if len(machines) > 0 {
+			beego.Trace("==========")
+			beego.Trace(user)
+			beego.Trace(machines)
+
+			// 2. Loop through user machines and get activations per user machine
+			for _, machine := range machines {
+
+				var sum int64
+
+				// Get activations for user and machine, get total time and other
+				query = fmt.Sprintf("SELECT SUM(time_total) FROM %s "+
+					"WHERE time_start>? AND time_end<? AND invoiced=false "+
+					"AND active=false AND user_id=? AND machine_id=?",
+					act.TableName())
+
+				err = o.Raw(query,
+					startTime.Format("2006-01-02"),
+					endTime.Format("2006-01-02"),
+					user.Id,
+					machine.Id).QueryRow(&sum)
+
+				if err != nil {
+					msg := fmt.Sprintf("Failed to get activation sum: %v", err)
+					return nil, errors.New(msg)
+				}
+
+				var final float32
+				var finalUnits string
+				if machine.PriceUnit == "minute" {
+					final = float32(sum) / 60.0
+					finalUnits = "minutes"
+				} else if machine.PriceUnit == "hour" {
+					final = float32(sum) / 60.0 / 60.0
+					finalUnits = "hours"
+				}
+
+				var price float32
+				price = machine.Price * final
+
+				beego.Trace("-----")
+				beego.Trace(machine.Name)
+				beego.Trace(final, finalUnits)
+				beego.Trace("price:", price)
+			}
+
+		}
+	}
+
+	filename := "files/MyXLSXFile.xlsx"
+	err = file.Save(filename)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+
+	inv := Invoice{}
+	inv.XlsFile = filename
+
+	return &inv, nil
 }
