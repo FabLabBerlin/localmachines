aa18c652dca9543ebb387912d2b3c20df9c959eb
kevinbacas <bacas.kevin@hotmail.fr>
2015-06-18 16:31:36 +0200

    Tests: Models: Memberships: Tests for Get and Delete function

--- tests/models/memberships/memberships_test.go
+++ tests/models/memberships/memberships_test.go
@@ -90,3 +90,45 @@ func TestCreateMembership(t *testing.T) {
 		})
 	})
 }
+
+func TestGetMembership(t *testing.T) {
+	Convey("Testing GetMembership", t, func() {
+		membershipName := "Lel"
+		Convey("Getting non-existing membership", func() {
+			_, err := models.GetMembership(0)
+
+			So(err, ShouldNotBeNil)
+		})
+		Convey("Creating a membership and getting it", func() {
+			mid, _ := models.CreateMembership(membershipName)
+			defer models.DeleteMembership(mid)
+			membership, err := models.GetMembership(mid)
+
+			So(membership.Id, ShouldEqual, mid)
+			So(err, ShouldBeNil)
+		})
+	})
+}
+
+func TestGetAllMemberships(t *testing.T) {
+	Convey("Testing GetAllMemberships", t, func() {
+		membershipName := "Lel"
+		Convey("Getting all memberships when there is nothing in the database", func() {
+			memberships, err := models.GetAllMemberships()
+
+			So(err, ShouldBeNil)
+			So(len(memberships), ShouldEqual, 0)
+		})
+		Convey("Creating 2 memberships and get them all", func() {
+			mid1, _ := models.CreateMembership(membershipName)
+			defer models.DeleteMembership(mid1)
+			mid2, _ := models.CreateMembership(membershipName)
+			defer models.DeleteMembership(mid2)
+
+			memberships, err := models.GetAllMemberships()
+
+			So(err, ShouldBeNil)
+			So(len(memberships), ShouldEqual, 2)
+		})
+	})
+}
