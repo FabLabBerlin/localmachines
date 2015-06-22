8c92cd0c18830b1ec2f1abb247e25b3ecdb9fe62
kevinbacas <bacas.kevin@hotmail.fr>
2015-06-22 13:01:26 +0200

    Tests: Updated all actual models with the config function

--- tests/models/memberships/memberships_test.go
+++ tests/models/memberships/memberships_test.go
@@ -1,65 +1,17 @@
 package membershipTests
 
 import (
-	"fmt"
-	"path/filepath"
-	"runtime"
 	"testing"
 
-	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/kr15h/fabsmith/models"
+	. "github.com/kr15h/fabsmith/tests/models"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func init() {
-	_, file, _, _ := runtime.Caller(1)
-	// find app.conf path
-	apppath, _ := filepath.Abs(filepath.Dir(filepath.Join(file, ".."+string(filepath.Separator)+".."+string(filepath.Separator)+".."+string(filepath.Separator))))
-	beego.TestBeegoInit(apppath)
-
-	// Setting log level
-	beego.SetLevel(beego.LevelError)
-
-	// Force Runmode to "test"
-	beego.RunMode = "test"
-
-	// Get MySQL config from environment variables
-	mysqlUser := beego.AppConfig.String("mysqluser")
-	if mysqlUser == "" {
-		panic("Please set mysqluser in app.conf")
-	}
-
-	mysqlPass := beego.AppConfig.String("mysqlpass")
-	if mysqlPass == "" {
-		panic("Please set mysqlpass in app.conf")
-	}
-
-	mysqlHost := beego.AppConfig.String("mysqlhost")
-	if mysqlHost == "" {
-		mysqlHost = "localhost"
-	}
-
-	mysqlPort := beego.AppConfig.String("mysqlport")
-	if mysqlPort == "" {
-		mysqlPort = "3306"
-	}
-
-	mysqlDb := beego.AppConfig.String("mysqldb")
-	beego.Info("Lel: " + mysqlDb)
-	if mysqlDb == "" {
-		panic("Please set mysqldb in app.conf")
-	}
-
-	// Build MySQL connection string out of the config variables
-	mysqlConnString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8",
-		mysqlUser, mysqlPass, mysqlHost, mysqlPort, mysqlDb)
-
-	// Register MySQL driver and default database for beego ORM
-	orm.RegisterDriver("mysql", orm.DR_MySQL)
-	orm.RegisterDataBase("default", "mysql", mysqlConnString)
+	ConfigDB()
 }
 
 func TestGetAllMemberships(t *testing.T) {
