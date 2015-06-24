2f0da45cbf313cfff8afa577733f4dede2d99fff
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-06-24 10:13:55 +0200

    Users: Fix a typo in main.js router

--- clients/user/src/js/main.js
+++ clients/user/src/js/main.js
@@ -14,6 +14,6 @@ let routes = (
     </Route>
 );
 
-Router.run(routes, Router.HistoryLocalion, function(Handler) {
+Router.run(routes, Router.HistoryLocation, function(Handler) {
     React.render(<Handler />, document.body);
 });
