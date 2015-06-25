cdbb568f4315e179a5014ff3a9a38072171d084c
kevinbacas <bacas.kevin@hotmail.fr>
2015-06-25 14:56:51 +0200

    Test: Controllers: Redirect: Added first controller test

---
+++ tests/controllers/mainredirect_test.go
@@ -0,0 +1,32 @@
+package test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	_ "github.com/kr15h/fabsmith/routers"
+
+	. "github.com/kr15h/fabsmith/tests/models"
+
+	"github.com/astaxie/beego"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func init() {
+	ConfigDB()
+}
+
+// TestMain is a sample to run an endpoint test
+func TestMain(t *testing.T) {
+
+	Convey("Test Station Endpoint", t, func() {
+		Convey("Status Code Should Be 302", func() {
+			r, _ := http.NewRequest("GET", "/", nil)
+			w := httptest.NewRecorder()
+			beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+			So(w.Code, ShouldEqual, 302)
+		})
+	})
+}
