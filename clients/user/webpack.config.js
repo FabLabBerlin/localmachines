bb0aa9a560837a20ff9d777a36abfecf50afd7f8
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-06-25 19:31:11 +0200

    User: Clean webpack config file, fix indentation

--- clients/user/webpack.config.js
+++ clients/user/webpack.config.js
@@ -1,28 +1,28 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './src/js/main.js',
-    output: {
-        path: __dirname + '/dev/assets',
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, exclude: /node_module/,  loader: 'babel' },
-            { test: /\.less$/, loader: "style!css!less" },
-            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=application/font-woff" },
-            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=application/font-woff" },
+  entry: './src/js/main.js',
+  output: {
+    path: __dirname + '/dev/assets',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/,  loader: 'babel' },
+      { test: /\.less$/, loader: "style!css!less" },
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=application/font-woff" },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=application/font-woff" },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=application/octet-stream" },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=image/svg+xml" }
-        ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-            "window.jQuery": "jquery",
-            "root.jQuery": "jquery"
-        })
     ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      "root.jQuery": "jquery"
+    })
+  ]
 };
