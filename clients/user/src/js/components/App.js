cf926d4e55508ebc9892ea7cad12ec3011f927e2
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-06-25 19:29:46 +0200

    User: Clean App.js
     - Add font-awesome logout icon
     - Fix indentation (use double-space instead of tab or 4-spaces)

--- clients/user/src/js/components/App.js
+++ clients/user/src/js/components/App.js
@@ -4,41 +4,40 @@ import React from 'react';
 import {Link, RouteHandler} from 'react-router';
 
 var App = React.createClass({
-    render: function() {
-        return (
-            <div className="app" >
-                <nav className="navbar navbar-default" >
-                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul className="nav navbar-nav" >
-                            <li>
-                                <img src="assets/logo_fablab_berlin.svg" className="brand-image" />
-                            </li>
-                        </ul>
-                        {UserStore.getIsLogged() ? (
-                            <ul className="nav navbar-nav navbar-right" >
-                                <li>
-                                    <button 
-                                        onClick={UserActions.logout}
-                                        className="btn btn-danger btn-lg"
-                                        >
-                                        Logout
-                                    </button>
-                                </li>
-                            </ul>
-                            ):('')}
-                        </div>
-                    </nav>
+  render: function() {
+    return (
+      <div className="app">
+        <nav className="navbar navbar-default">
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav" >
+              <li>
+                <img src="assets/logo_fablab_berlin.svg" className="brand-image" />
+              </li>
+            </ul>
+            {UserStore.getIsLogged() ? (
+              <ul className="nav navbar-nav navbar-right" >
+                <li>
+                  <button 
+                    onClick={UserActions.logout}
+                    className="btn btn-danger btn-lg">
+                    <i className="fa fa-sign-out"></i>
+                  </button>
+                </li>
+              </ul>
+            ):('')}
+          </div>
+        </nav>
 
-                    <RouteHandler />
+        <RouteHandler />
 
-                    <footer className="absolute-bottom" >
-                        <div className="container-fuild">
-                            <i className="fa fa-copyright"></i> Fab Lab Berlin 2015
-                        </div>
-                    </footer>
-                </div>
-        );
-    }
+        <footer className="absolute-bottom">
+          <div className="container-fuild">
+            <i className="fa fa-copyright"></i> Fab Lab Berlin 2015
+          </div>
+        </footer>
+      </div>
+    );
+  }
 });
 
 module.exports = App;
