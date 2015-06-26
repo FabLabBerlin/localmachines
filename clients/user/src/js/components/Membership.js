8eb98f2433492ca4cedde0825486df39e6d27cba
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-06-26 12:38:22 +0200

    User: Reindent Membership component

--- clients/user/src/js/components/Membership.js
+++ clients/user/src/js/components/Membership.js
@@ -1,31 +1,35 @@
 import React from 'react';
 
 var Membership = React.createClass({
-    render() {
-        if(this.props.info.length != 0) {
-            var membership = this.props.info[0];
-            var MembershipNode = 
-                <tr>
-                    <td> Membership id: {membership.MembershipId}</td>
-                    <td>Start date: {membership.StartDate}</td>
-                </tr>
-        } else {
-            var MembershipNode = <tr>You do not have any membership</tr>
-        }
-        return (
-            <table className="table table-striped table-hover" >
-                <thead>
-                    <tr>
-                        <th>Membership Id</th>
-                        <th>Start date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {MembershipNode}
-                </tbody>
-            </table>
-        );
+  render() {
+    var MembershipNode = ( 
+      <tr>
+        <td>You do not have any memberships</td>
+      </tr> 
+    );
+    if(this.props.info.length != 0) {
+      var membership = this.props.info[0];
+      MembershipNode = (
+        <tr>
+          <td> Membership id: {membership.MembershipId}</td>
+          <td>Start date: {membership.StartDate}</td>
+        </tr>
+      );
     }
+    return (
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Membership Id</th>
+            <th>Start date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {MembershipNode}
+        </tbody>
+      </table>
+    );
+  }
 });
 
 module.exports = Membership;
