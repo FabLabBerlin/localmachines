acf1705a9b823cc7f1172169fcd4af64f769dc6a
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-06-26 12:07:12 +0200

    User: Reindent and improve UserForm component

--- clients/user/src/js/components/UserForm.js
+++ clients/user/src/js/components/UserForm.js
@@ -3,66 +3,75 @@ import React from 'react';
 
 var UserForm = React.createClass({
 
-    // Pass changing of the input responsability to the parent
-    handleChangeForm(event) {
-        this.props.func(event);
-    },
+  // Pass changing of the input responsability to the parent
+  handleChangeForm(event) {
+    this.props.func(event);
+  },
 
-    // Pass the submit responsability to the parent
-    handleSubmit() {
-        // should sent value to useractions
-        this.props.submit();
-    },
+  // Pass the submit responsibility to the parent
+  handleSubmit() {
+    // should send value to useractions
+    this.props.submit();
+  },
 
-    render() {
-        var NodeInput = _.map(this.props.info, function(value, key) {
-            return (
-                <div className="col-md-6" key={key}>
-                    <div className="form-group">
-                        <label htmlFor="user-information" >{key}</label>
-                        <input type="text" value={value} 
-                            id={key}
-                            className="form-control"
-                            onChange={this.handleChangeForm}
-                        />
-                    </div>
-                </div>
-            );
-        }.bind(this));
-        return (
-            //l61, put font-awesome to get the logo
-            <div className="" >
-                <form onSubmit={this.handleSubmit} >
-                    <div className="row" >
-                        {NodeInput}
-                    </div>
-                    <div className="row" >
-                        <div className="form-group" >
-                            <div className="col-sm-6" >
-                                <label htmlFor="user-password" >User Password </label>
-                                <input 
-                                    type="password" className="form-control"
-                                    placeholder="new password"
-                                />
-                            </div>
-                            <div className="col-sm-6" >
-                                <label htmlFor="user-password" >User Password </label>
-                                <input 
-                                    type="password" className="form-control"
-                                    placeholder="repeat password"
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-6" >
-                        <button className="btn btn-primary btn-lg">
-                            Save
-                        </button>
-                    </div>
-                </form>
+  render() {
+    var NodeInput = _.map(this.props.info, function(value, key) {
+      return (
+        <div className="col-md-6" key={key}>
+          <div className="form-group">
+            <label htmlFor="user-information">{key}</label>
+            <input type="text" value={value} 
+              id={key}
+              className="form-control"
+              onChange={this.handleChangeForm}
+            />
+          </div>
+        </div>
+      );
+    }.bind(this));
+    return (
+      //l61, put font-awesome to get the logo
+      <form onSubmit={this.handleSubmit}>
+
+        <div className="row">
+          {NodeInput}
+        </div>
+
+        <div className="row">
+          <div className="form-group">
+            
+            <div className="col-sm-6">
+              <label htmlFor="user-password">User Password </label>
+              <input 
+                type="password" className="form-control"
+                placeholder="new password"
+              />
             </div>
-        );
-    }
+            
+            <div className="col-sm-6">
+              <label htmlFor="user-password">User Password </label>
+              <input 
+                type="password" className="form-control"
+                placeholder="repeat password"
+              />
+            </div>
+
+          </div>
+        </div>
+
+        <hr />
+        
+        <div className="clearfix">
+          <div className="pull-right">
+            <button className="btn btn-primary">
+              <i className="fa fa-save"></i> Save
+            </button>
+          </div>
+        </div>
+
+      </form>
+    );
+  }
 });
 
 module.exports = UserForm;
