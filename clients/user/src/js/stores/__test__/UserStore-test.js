833399425a978ea169925cf9ff166edaad013780
LouisDelbosc <louisdelbosc@gmail.com>
2015-07-07 12:32:00 +0200

    User: test a part of api call

--- clients/user/src/js/stores/__test__/UserStore-test.js
+++ clients/user/src/js/stores/__test__/UserStore-test.js
@@ -1,13 +1,20 @@
 jest.dontMock('../UserStore.js');
 
 describe('UserStore test', function() {
-  it('call into $.ajax with the correct params', function() {
+
+  /*
+   * Test API calls
+   * without params or only uid
+   */
+  it('call into $.ajax with no param or uid', function() {
     var $ = require('jquery');
-
     var UserStore = require('../UserStore');
+    var uidTest = 3;
 
+    /*
+     * Test logout
+     */
     UserStore.logoutFromServer();
-
     expect($.ajax).toBeCalledWith({
       cache: false,
       dataType: 'json',
@@ -16,5 +23,42 @@ describe('UserStore test', function() {
       success: jasmine.any(Function),
       error: jasmine.any(Function)
     });
+
+    /*
+     * Test getUserStateFromServer
+     */
+    UserStore.getUserStateFromServer(uidTest);
+    expect($.ajax).toBeCalledWith({
+      url: '/api/users/3',
+      dataType: 'json',
+      type: 'GET',
+      success: jasmine.any(Function),
+      error: jasmine.any(Function)
+    });
+
+    /*
+     * Test getMachineFromServer
+     */
+    UserStore.getMachineFromServer(uidTest);
+    expect($.ajax).toBeCalledWith({
+      url: '/api/users/3/machinepermissions',
+      dataType: 'json',
+      type: 'GET',
+      success: jasmine.any(Function),
+      error: jasmine.any(Function)
+    });
+
+    /*
+     * Test getMembershipFromServer
+     */
+    UserStore.getMembershipFromServer(uidTest);
+    expect($.ajax).toBeCalledWith({
+      url: '/api/users/3/machinepermissions',
+      dataType: 'json',
+      type: 'GET',
+      success: jasmine.any(Function),
+      error: jasmine.any(Function)
+    });
   });
+
 });
