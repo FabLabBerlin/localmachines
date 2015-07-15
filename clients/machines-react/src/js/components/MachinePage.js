f162ee4ee55c087de197efcfbbbc4348b78a3334
LouisDelbosc <louisdelbosc@gmail.com>
2015-07-15 16:17:06 +0200

    Machine: change the state place in store

--- clients/machines-react/src/js/components/MachinePage.js
+++ clients/machines-react/src/js/components/MachinePage.js
@@ -1,68 +1,29 @@
 import React from 'react'
 import MachineList from './MachineList';
+import MachineStore from '../stores/MachineStore';
 
 var MachinePage = React.createClass({
-  getUserInfo() {
-    return {
-      uid: 3,
-      FirstName: 'test',
-      LastName: 'test'
-    }
-  },
-
-  getAcInfo() {
-    return [
-      {
-        Id: 1,
-        UserId: 3,
-        MachineId: 1,
-        TimeStart: "2015-07-14T18:17:28+02:00"
-      },
-      {
-        Id: 2,
-        UserId: 2,
-        MachineId: 3,
-        TimeStart: "2015-07-14T18:17:28+02:00"
-      }
-    ]
-  },
-
-  getMachineInfo() {
-    return [
-      {
-        Id: 1,
-        Name: "Laydrop 3D Printer",
-        Image: "",
-        ConnectedMachines: "[3]",
-        SwitchRefCount: 0
-      },
-      {
-        Id: 3,
-        Name: "Shitty Machine I dunno the name",
-        Image: "",
-        ConnectedMachines: "[2]",
-        SwitchRefCount: 0
-      },
-      {
-        Id: 2,
-        Name: "MakerBot 3D Printer",
-        Image: "machine-2.svg",
-        ConnectedMachines: "",
-        SwitchRefCount: 0
-      }
-    ]
-  },
 
   getInitialState() {
     return {
-      userInfo: this.getUserInfo(),
-      machineInfo: this.getMachineInfo(),
-      activationInfo: this.getAcInfo()
+      userInfo: MachineStore.getUserInfo(),
+      machineInfo: MachineStore.getMachineInfo(),
+      activationInfo: MachineStore.getActivationInfo()
     };
   },
 
   getUserId() {
     return this.state.userInfo.uid;
+  },
+
+  onChangeActivation() {
+    this.setState({
+      activationInfo: MachineStore.getActivationInfo()
+    });
+  },
+
+  componentDidMount() {
+    MachineStore.onChangeActivation = this.onChangeActivation;
   },
 
   render() {
