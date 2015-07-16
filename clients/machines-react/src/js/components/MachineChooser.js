ae13af7c9291a6286d583586f12c93b1db97f0cb
Philip Silva <psilva.de@gmail.com>
2015-07-16 20:40:28 +0200

    MachineChooser: indicate available machine with green

--- clients/machines-react/src/js/components/MachineChooser.js
+++ clients/machines-react/src/js/components/MachineChooser.js
@@ -15,40 +15,43 @@ var MachineChooser = React.createClass({
     console.log('sameUser ?');
     console.log(this.props.sameUser);
     */
+    var available = !this.props.busy && !this.props.sameUser;
     return (
-      <div>
-        <div className="machine-header">
-          <div className="machine-title pull-left">{this.props.info.Name}</div>
-          <div className="machine-info-btn pull-right">
-            
-            <a className="machine-info-toggle" onClick={this.toggleInfo}>
-              <span className="glyphicon glyphicon-info-sign" ng-class="{hidden: infoVisible}"></span>
-              <span className="glyphicon glyphicon-remove-circle" ng-class="{hidden: !infoVisible}"></span>
-            </a>
+      <div className={'machine ' + (available ? 'available' : '')}>
+        <div className="container-fluid">
+          <div className="machine-header">
+            <div className="machine-title pull-left">{this.props.info.Name}</div>
+            <div className="machine-info-btn pull-right">
+              
+              <a className="machine-info-toggle" onClick={this.toggleInfo}>
+                <span className="glyphicon glyphicon-info-sign" ng-class="{hidden: infoVisible}"></span>
+                <span className="glyphicon glyphicon-remove-circle" ng-class="{hidden: !infoVisible}"></span>
+              </a>
 
+            </div>
+            <div className="clearfix"></div>
+            <div className="machine-info-content">{this.props.info.Description}</div>
           </div>
-          <div className="clearfix"></div>
-          <div className="machine-info-content">{this.props.info.Description}</div>
-        </div>
-        <div className="machine-body">
-          { this.props.busy ?
-            this.props.sameUser ? (
-            <BusyMachine
-              activation={this.props.activation}
+          <div className="machine-body">
+            { this.props.busy ?
+              this.props.sameUser ? (
+              <BusyMachine
+                activation={this.props.activation}
+                info={this.props.info}
+              />
+              ) : (
+              <OccupiedMachine
+                activation={this.props.activation}
+                info={this.props.info}
+                uid={this.props.uid}
+              />
+              ) :
+                (
+            <FreeMachine
               info={this.props.info}
             />
-            ) : (
-            <OccupiedMachine
-              activation={this.props.activation}
-              info={this.props.info}
-              uid={this.props.uid}
-            />
-            ) :
-              (
-          <FreeMachine
-            info={this.props.info}
-          />
-          )}
+            )}
+          </div>
         </div>
       </div>
     );
