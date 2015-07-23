364e8dc56e227b4ed8656f2a58d8c434b1e495e5
LouisDelbosc <louisdelbosc@gmail.com>
2015-07-23 12:44:04 +0200

    Machine: add loginnfc

--- clients/machines-react/src/js/components/LoginChooser.js
+++ clients/machines-react/src/js/components/LoginChooser.js
@@ -2,6 +2,11 @@ import React from 'react';
 import LoginNfc from './LoginNfc';
 import Login from './Login';
 
+/*
+ * LoginChooser
+ * Will choose the right login page
+ * depending of the presence of window.libnfc
+ */
 var LoginChooser = React.createClass({
   render() {
     return (
--- clients/machines-react/src/js/components/LoginNfc.js
+++ clients/machines-react/src/js/components/LoginNfc.js
@@ -1,6 +1,51 @@
 import React from 'react';
+import {Navigation} from 'react-router';
+import LoginStore from '../stores/LoginStore';
+import LoginActions from '../actions/LoginActions';
 
+/*
+ * LoginNfc
+ * Component to Login if there is window.libnfc
+ */
 var LoginNfc = React.createClass({
+
+  mixins: [ Navigation ],
+
+  errorNFCCallback(error) {
+    window.libnfc.cardRead.disconnect(this.nfcLogin);
+    window.libnfc.cardReaderError.disconnect(this.errorNFCCallback);
+    toastr.error(error);
+    setTimeout(this.getNFCUid, 2000);
+  },
+
+  nfcLogin(uid) {
+    LoginActions.nfcLogin(uid);
+  },
+
+  getNFCUid() {
+    window.libnfc.cardRead.connect(this.nfcLogin);
+    window.libnfc.cardReaderError.connect(this.errorNFCCallback)
+    window.libnfc.asyncScan();
+  },
+
+  onChangeLoginNFC() {
+    if( LoginStore.getIsLogged() ) {
+      this.replaceWith('/machine');
+    } else {
+      setTimeout(this.getNFCUid, 1000);
+    }
+  },
+
+  componentWillUnmount() {
+    window.libnfc.cardRead.disconnect(this.nfcLogin);
+    window.libnfc.cardReaderError.disconnect(this.errorNFCCallback);
+  },
+
+  componentDidMount() {
+    setTimeout(this.getNFCUid, 1000);
+    LoginStore.onChangeLoginNFC = this.onChangeLogin;
+  },
+
   render() {
     return (
       <form className="login-form" >
