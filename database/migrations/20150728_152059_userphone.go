1d5ebfecdb01915447c3971c25ca8509b145d9f7
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-07-28 15:29:07 +0200

    Add database migration: phone number col in user table

---
+++ database/migrations/20150728_152059_userphone.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/astaxie/beego/migration"
+)
+
+// DO NOT MODIFY
+type Userphone_20150728_152059 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &Userphone_20150728_152059{}
+	m.Created = "20150728_152059"
+	migration.Register("Userphone_20150728_152059", m)
+}
+
+func (m *Userphone_20150728_152059) Up() {
+	m.Sql("ALTER TABLE user ADD COLUMN phone VARCHAR(50)")
+}
+
+func (m *Userphone_20150728_152059) Down() {
+	m.Sql("ALTER TABLE user DROP COLUMN phone")
+}
