53b8941aa81939fd5fb49354350f4af649183357
Philip Silva <psilva.de@gmail.com>
2015-08-17 18:04:07 +0200

    Only keeping new Machines Client

---
+++ clients/machines/src/js/stores/__test__/LoginStore-test.js
@@ -0,0 +1,46 @@
+jest.dontMock('nuclear-js');
+jest.dontMock('../../flux');
+jest.dontMock('../../getters');
+jest.dontMock('../LoginStore.js');
+jest.mock('jquery');
+jest.mock('toastr');
+
+var $ = require('jquery');
+var actionTypes = require('../../actionTypes');
+var Flux = require('../../flux');
+var getters = require('../../getters');
+var LoginStore = require('../LoginStore');
+
+
+describe('LoginStore', function() {
+  Flux.registerStores({
+    loginStore: LoginStore
+  });
+
+  describe('SUCCESS_LOGOUT', function() {
+    it('sets store into logout state', function() {
+      Flux.dispatch(actionTypes.SUCCESS_LOGOUT);
+      const isLogged = Flux.evaluateToJS(getters.getIsLogged);
+      expect(isLogged).toBe(false);
+    });
+  });
+
+  describe('SUCCESS_LOGIN', function() {
+    it('sets store into logged in state', function() {
+      var data = {
+        UserId: 123
+      };
+      Flux.dispatch(actionTypes.SUCCESS_LOGIN, { data });
+      const uid = Flux.evaluateToJS(getters.getUid);
+      expect(uid).toBe(123);
+    });
+  });
+
+  describe('ERROR_LOGIN', function() {
+    it('sets firstTry to false', function() {
+      Flux.dispatch(actionTypes.ERROR_LOGIN);
+      const firstTry = Flux.evaluateToJS(getters.getFirstTry);
+      expect(firstTry).toBe(false);
+    });
+  });
+});
---
+++ clients/machines/src/js/stores/__test__/MachineStore-test.js
@@ -0,0 +1,95 @@
+jest.dontMock('nuclear-js');
+jest.dontMock('../../actionTypes');
+jest.dontMock('../../flux');
+jest.dontMock('../../getters');
+jest.dontMock('lodash');
+jest.dontMock('../LoginStore.js');
+jest.dontMock('../MachineStore.js');
+jest.mock('jquery');
+
+var actionTypes = require('../../actionTypes');
+var Flux = require('../../flux');
+var getters = require('../../getters');
+
+
+function activation() {
+  return [
+    {
+      FirstName: 'Regular',
+      Id: 227,
+      LastName: 'Admin',
+      MachineId: 2,
+      TimeTotal: 1,
+      UserId: 2
+    }
+  ];
+}
+
+function machines() {
+  return [
+    {
+      Id: 1,
+      Name: 'Printer5000'
+    },
+    {
+      Id: 2,
+      Name: 'Form1'
+    }
+  ];
+}
+
+function user() {
+  return {
+    FirstName: 'Regular',
+    Id: 2,
+    LastName: 'Admin',
+    UserRole: 'admin'
+  };
+}
+
+describe('MachineStore', function() {
+  var $ = require('jquery');
+  var LoginStore = require('../LoginStore');
+  var MachineStore = require('../MachineStore');
+
+  Flux.registerStores({
+    loginStore: LoginStore,
+    machineStore: MachineStore
+  });
+
+  describe('SET_ACTIVATION_INFO', function() {
+    it('does changes visible via getActivationInfo', function() {
+      var activationInfo = activation();
+      Flux.dispatch(actionTypes.SET_ACTIVATION_INFO, { activationInfo });
+      var actual = Flux.evaluateToJS(getters.getActivationInfo);
+      expect(actual).toEqual(activation());
+    });
+  });
+
+  describe('SET_USER_INFO', function() {
+    it('does changes visible via getUserInfo', function() {
+      var userInfo = user();
+      Flux.dispatch(actionTypes.SET_USER_INFO, { userInfo });
+      var actual = Flux.evaluateToJS(getters.getUserInfo);
+      expect(actual).toEqual(user());
+    });
+  });
+
+  describe('SET_MACHINE_INFO', function() {
+    it('does changes visible via getMachineInfo', function() {
+      var machineInfo = machines();
+      Flux.dispatch(actionTypes.SET_MACHINE_INFO, { machineInfo });
+      var actual = Flux.evaluateToJS(getters.getMachineInfo);
+      expect(actual).toEqual(machines());
+    });
+  });
+
+  describe('MACHINE_STORE_CLEAR_STATE', function() {
+    it('clears the state', function() {
+      Flux.dispatch(actionTypes.MACHINE_STORE_CLEAR_STATE);
+      expect(Flux.evaluateToJS(getters.getUserInfo)).toEqual({});
+      expect(Flux.evaluateToJS(getters.getActivationInfo)).toEqual([]);
+      expect(Flux.evaluateToJS(getters.getMachineInfo)).toEqual([]);
+    });
+  });
+});
