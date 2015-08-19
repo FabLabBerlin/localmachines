34ffa21855f1277f6aae95cf949ae02487de2abe
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-08-19 14:02:57 +0200

    Machines: Tests: Add NFC login view test

--- clients/machines/src/js/components/__test__/Login-test.js
+++ clients/machines/src/js/components/__test__/Login-test.js
@@ -1,16 +1,34 @@
 jest.mock('jquery');
 jest.mock('../../actions/LoginActions.js');
 jest.dontMock('../Login.js');
+jest.dontMock('../LoginChooser.js');
+jest.dontMock('../LoginNfc.js');
 
-
-describe('Login', function() {
-  it('renders Log In and Sign Up', function() {
+describe('LoginChooser', function() {
+  it('renders Log In and Sign up in case of normal browser', function() {
     var React = require('react');
-    var Login = React.createFactory(require('../Login'));
-    var login = new Login({});
-    var s = React.renderToString(login);
+    var LoginChooser = React.createFactory(require('../LoginChooser'));
+    var loginChooser = new LoginChooser({});
+    var s = React.renderToString(loginChooser);
     expect(s).toContain('Log In');
     expect(s).toContain('Sign up');
+  });
+
+  it('renders NFC icon and related text in case of NFC browser', function() {
+    window.libnfc = {
+      debug: true,
+      cardRead: { connect: function() {} },
+      cardReaderError: { connect: function() {} },
+      asyncScan: function() {}
+    };
+    var React = require('react');
+    var LoginChooser = React.createFactory(require('../LoginChooser'));
+    var loginChooser = new LoginChooser({});
+    var s = React.renderToString(loginChooser);
+    expect(s).toContain('class="nfc-login-info-icon"');
+    expect(s).toContain('class="nfc-login-info-text"');
+    expect(s).toContain('Use your NFC card to log in');
+    window.libnfc = null;
   });
 
   it('does something when clicking Log In', function() {
@@ -48,4 +66,5 @@ describe('Login', function() {
       password: '123456'
     });
   });
+
 });
