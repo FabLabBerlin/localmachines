42640cb86ba41b6a2f652125d7bae2e046dd573a
Philip Silva <psilva.de@gmail.com>
2015-08-31 17:06:03 +0200

    Machines Client: Header Nav: highlighting active button

--- clients/machines/src/js/components/HeaderNav.js
+++ clients/machines/src/js/components/HeaderNav.js
@@ -1,13 +1,28 @@
 var React = require('react');
 
 
+var Button = React.createClass({
+  render() {
+    var className;
+    if (this.props.href === (window.location.pathname + window.location.hash)) {
+      className = 'header-nav-active';
+    }
+    return (
+      <a id={this.props.id} className={className} href={this.props.href}>
+        {this.props.children}
+      </a>
+    );
+  }
+});
+
+
 var HeaderNav = React.createClass({
   render() {
     return (
       <div className="header-nav">
-        <a id="header-nav-machines" href="/machines/#/machine">Machines</a>
-        <a id="header-nav-profile" href="/machines/#/profile">Profile</a>
-        <a id="header-nav-spendings" href="/machines/#/spendings">Spendings</a>
+        <Button id="header-nav-machines" href="/machines/#/machine">Machines</Button>
+        <Button id="header-nav-profile" href="/machines/#/profile">Profile</Button>
+        <Button id="header-nav-spendings" href="/machines/#/spendings">Spendings</Button>
       </div>
     );
   }
