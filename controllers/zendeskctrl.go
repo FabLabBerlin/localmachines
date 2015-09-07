22e3fa83e9b3c647c2cae486161df879425e5984
Philip Silva <psilva.de@gmail.com>
2015-09-07 16:09:40 +0200

    ZenDesk model and controller

---
+++ controllers/zendeskctrl.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/kr15h/fabsmith/models"
+)
+
+type FeedbackController struct {
+	Controller
+}
+
+// @Title CreateTicket
+// @Description Create ZenDesk ticket
+// @Param name     query   string  true     "Full name of the customer"
+// @Param email    query   string  true     "Email of the customer"
+// @Param subject  query   string  true     "Subject of the Ticket"
+// @Param message  query   string  true     "Message of the Ticket"
+// @Success 200 {object} models.FastBillCreateCustomerResponse
+// @Failure 500 Internal Server Error
+// @Failure 401 Not authorized
+// @router  /feedback [post]
+func (this *FeedbackController) PostFeedback() {
+	zd := models.ZenDesk{
+		Email:    beego.AppConfig.String("zendeskemail"),
+		ApiToken: beego.AppConfig.String("zendeskapitoken"),
+		BaseUrl:  beego.AppConfig.String("zendeskbaseurl"),
+	}
+
+	ticket := models.ZenDeskTicket{
+		Requester: models.ZenDeskTicketRequester{
+			Name:  this.GetString("name"),
+			Email: this.GetString("email"),
+		},
+		Subject: this.GetString("subject"),
+		Comment: models.ZenDeskTicketComment{
+			Body: this.GetString("message"),
+		},
+	}
+	if err := zd.SubmitTicket(ticket); err == nil {
+		this.ServeJson()
+	} else {
+		beego.Error("Error submitting ZenDesk Ticket: ", err)
+		this.CustomAbort(500, "Error submitting Ticket")
+	}
+}
---
+++ models/zendesk.go
@@ -0,0 +1,75 @@
+package models
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/astaxie/beego"
+	"io/ioutil"
+	"net/http"
+)
+
+type ZenDesk struct {
+	Email    string
+	ApiToken string
+	BaseUrl  string // https://<subdomain>.zendesk.com
+}
+
+func (this ZenDesk) ApiUrl(function string) string {
+	return this.BaseUrl + "/api/v2/" + function
+}
+
+func (this ZenDesk) SubmitTicket(ticket ZenDeskTicket) error {
+	request := ZenDeskTicketRequest{
+		Ticket: ticket,
+	}
+	jsonBytes, err := json.Marshal(request)
+	if err != nil {
+		return fmt.Errorf("Failed to marshal JSON: %v", err)
+	}
+	req, err := http.NewRequest("POST", this.ApiUrl("tickets.json"), bytes.NewBuffer(jsonBytes))
+	if err != nil {
+		return fmt.Errorf("Failed to create request: %v", err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.SetBasicAuth(this.Email, this.ApiToken)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		beego.Error("Failed to get response:", err)
+		return fmt.Errorf("Failed to get response")
+	}
+	defer resp.Body.Close()
+
+	var body []byte
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		beego.Error("Failed to read response body:", err)
+		return fmt.Errorf("Failed to read response body")
+	}
+
+	if resp.StatusCode > 299 {
+		return fmt.Errorf("unexpected Status Code %v: %v", resp.StatusCode, string(body))
+	}
+
+	return nil
+}
+
+type ZenDeskTicketRequest struct {
+	Ticket ZenDeskTicket `json:"ticket"`
+}
+
+type ZenDeskTicket struct {
+	Requester ZenDeskTicketRequester `json:"requester"`
+	Subject   string                 `json:"subject"`
+	Comment   ZenDeskTicketComment   `json:"comment"`
+}
+
+type ZenDeskTicketRequester struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type ZenDeskTicketComment struct {
+	Body string `json:"body"`
+}
