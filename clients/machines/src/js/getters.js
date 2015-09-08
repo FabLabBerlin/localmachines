3204d3c6bdaa79d9c20eb53db654fc8167940fcc
Philip Silva <psilva.de@gmail.com>
2015-09-08 19:43:22 +0200

    Machines Client: Bill Table: moving (business) logic from View into getters; also using getters more NuclearJS-style (for example getter dependencies in getBillMonths)

--- clients/machines/src/js/getters.js
+++ clients/machines/src/js/getters.js
@@ -1,6 +1,6 @@
 var _ = require('lodash');
+var { toCents, subtractVAT } = require('./components/UserProfile/helpers');
 var moment = require('moment');
-var reactor = require('./reactor');
 
 
 /*
@@ -45,21 +45,46 @@ const getLastActivity = [
 /*
  * User (Page) related getters
  */
- const getBillInfo = [
+const getUserInfo = [
+  ['userStore'],
+  (userStore) => {
+    return userStore.get('userInfo');
+  }
+];
+
+const getBillInfo = [
   ['userStore'],
   (userStore) => {
     return userStore.get('billInfo');
   }
- ];
+];
 
- const getMembership = [
+const getBillMonths = [
+  getUserInfo,
+  (userInfo) => {
+    var months = [];
+    var created = moment(userInfo.get('Created'));
+    if (created.unix() <= 0) {
+      created = moment('2015-07-01');
+    }
+    for (var t = created; t.isBefore(moment()); t.add(1, 'd')) {
+      months.push(t.clone());
+    }
+    months = _.uniq(months, function(month) {
+      return month.format('MMM YYYY');
+    });
+    return months;
+  }
+];
+
+const getMembership = [
   ['userStore'],
   (userStore) => {
     return userStore.get('membershipInfo');
   }
- ];
+];
 
- const getMembershipsByMonth = [
+const getMembershipsByMonth = [
   ['userStore'],
   (userStore) => {
     var byMonths = {};
@@ -76,16 +101,86 @@ const getLastActivity = [
     });
     return byMonths;
   }
- ];
-
-
- const getUserInfo = [
-  ['userStore'],
-  (userStore) => {
-    return userStore.get('userInfo');
-  }
 ];
 
+const getMonthlyBills = [
+  getBillInfo,
+  getBillMonths,
+  getMembershipsByMonth,
+  (billInfo, billMonths, membershipsByMonth) => {
+    var activations = billInfo.Activations;
+    var activationsByMonth = _.groupBy(activations, function(info) {
+      return moment(info.TimeStart).format('MMM YYYY');
+    });
+    var monthlyBills = _.map(billMonths, function(m) {
+      var month = m.format('MMM YYYY');
+      var monthlyBill = {
+        month: month,
+        activations: [],
+        memberships: [],
+        sums: {
+          activations: {
+            priceInclVAT: 0,
+            priceExclVAT: 0,
+            priceVAT: 0,
+            durations: 0
+          },
+          memberships: {
+            priceInclVAT: 0,
+            priceExclVAT: 0,
+            priceVAT: 0
+          },
+          total: {}
+        }
+      };
+
+      _.eachRight(activationsByMonth[month], function(info) {
+        var duration = moment.duration(moment(info.TimeEnd).diff(moment(info.TimeStart))).asSeconds();
+        monthlyBill.sums.durations += duration;
+        var priceInclVAT = toCents(info.DiscountedTotal);
+        var priceExclVAT = toCents(subtractVAT(info.DiscountedTotal));
+        var priceVAT = priceInclVAT - priceExclVAT;
+        monthlyBill.sums.activations.priceInclVAT += priceInclVAT;
+        monthlyBill.sums.activations.priceExclVAT += priceExclVAT;
+        monthlyBill.sums.activations.priceVAT += priceVAT;
+        monthlyBill.activations.push({
+          MachineName: info.MachineName,
+          TimeStart: moment(info.TimeStart),
+          duration: duration,
+          priceExclVAT: priceExclVAT,
+          priceVAT: priceVAT,
+          priceInclVAT: priceInclVAT
+        });
+      });
+
+      _.each(membershipsByMonth[month], function(membership) {
+        var totalPrice = toCents(membership.MonthlyPrice);
+        var priceExclVat = toCents(subtractVAT(membership.MonthlyPrice));
+        var vat = totalPrice - priceExclVat;
+        monthlyBill.memberships.push({
+          startDate: moment(membership.StartDate),
+          endDate: moment(membership.StartDate).add(membership.Duration, 'd'),
+          priceExclVAT: priceExclVat,
+          priceVAT: vat,
+          priceInclVAT: totalPrice
+        });
+        monthlyBill.sums.memberships.priceInclVAT += totalPrice;
+        monthlyBill.sums.memberships.priceExclVAT += priceExclVat;
+        monthlyBill.sums.memberships.priceVAT += vat;
+      });
+
+      monthlyBill.sums.total = {
+        priceInclVAT: monthlyBill.sums.activations.priceInclVAT + monthlyBill.sums.memberships.priceInclVAT,
+        priceExclVAT: monthlyBill.sums.activations.priceExclVAT + monthlyBill.sums.memberships.priceExclVAT,
+        priceVAT: monthlyBill.sums.activations.priceVAT + monthlyBill.sums.memberships.priceVAT
+      };
+
+      return monthlyBill;
+    });
+    monthlyBills.reverse();
+    return monthlyBills;
+  }
+];
 
 /*
  * Machine (Page) related getters
@@ -198,7 +293,7 @@ const getScrollPosition = [
 
 export default {
   getIsLogged, getUid, getFirstTry, getLoginSuccess, getLastActivity,
-  getUserInfo, getActivationInfo, getMachineInfo, getMachine, getMachineUsers, getIsLoading, getBillInfo, getMembership, getMembershipsByMonth,
+  getUserInfo, getActivationInfo, getMachineInfo, getMachine, getMachineUsers, getIsLoading, getBillInfo, getBillMonths, getMonthlyBills, getMembership, getMembershipsByMonth,
   getFeedbackSubject, getFeedbackSubjectDropdown, getFeedbackSubjectOtherText, getFeedbackMessage,
   getScrollUpEnabled, getScrollDownEnabled, getScrollPosition
 };
