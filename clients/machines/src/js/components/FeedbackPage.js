fc026f396db3a068e816ef3e1bbbfb8cf57526ba
Philip Silva <psilva.de@gmail.com>
2015-09-08 09:31:28 +0200

    Machines Client: Feedback form: minimum styling

--- clients/machines/src/js/components/FeedbackPage.js
+++ clients/machines/src/js/components/FeedbackPage.js
@@ -53,42 +53,49 @@ var FeedbackPage = React.createClass({
 
   render() {
     return (
-      <div>
-        <div>
-          <label>
-            Subject:
-            <select id="subject-dropdown"
-                    onChange={this.handleChange}
-                    value={this.state.subjectDropdown}>
-              <option>Billing</option>
-              <option>Technical</option>
-              <option>Other</option>
-            </select>
+      <div className="container">
+        <h3>Your Feedback</h3>
+        <div className="form-group">
+          <label htmlFor="subject-dropdown">
+            Subject
           </label>
+          <select id="subject-dropdown"
+                  className="form-control"
+                  onChange={this.handleChange}
+                  value={this.state.subjectDropdown}>
+            <option>Billing</option>
+            <option>Technical</option>
+            <option>Other</option>
+          </select>
         </div>
         {this.state.subjectDropdown === 'Other' ?
           (
-            <div>
-              <label>
-                Other text:
+            <div className="form-group">
+              <div>
+                <label htmlFor="subject-other-text">
+                  Other text
+                </label>
                 <input id="subject-other-text"
+                       className="form-control"
                        onChange={this.handleChange}
                        value={this.state.subjectOtherText}/>
-              </label>
+              </div>
             </div>
           ) : null
         }
-        <div>
-          <label>
-            Message:
-            <textarea id="message"
-                      onChange={this.handleChange}
-                      value={this.state.message}
-            />
+        <div className="form-group">
+          <label htmlFor="message">
+            Message
           </label>
+          <textarea id="message"
+                    className="form-control"
+                    onChange={this.handleChange}
+                    value={this.state.message}
+          />
         </div>
         <div>
-          <button onClick={this.handleSubmit}>Submit</button>
+          <button className="btn btn-primary btn-lg"
+                  onClick={this.handleSubmit}>Submit</button>
         </div>
       </div>
     );
