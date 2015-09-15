1fac089ef9e99d884bf6150e407821bab463898e
Philip Silva <psilva.de@gmail.com>
2015-09-15 09:27:28 +0200

    Machines: RepairButton test

---
+++ clients/machines/src/js/components/Feedback/__test__/RepairButton-test.js
@@ -0,0 +1,21 @@
+jest.dontMock('../RepairButton');
+
+
+var FeedbackDialogs = require('../../Feedback/FeedbackDialogs');
+var RepairButton = require('../RepairButton');
+
+
+describe('RepairButton', function() {
+	describe('render', function() {
+    it('renders the button and makes it clickable', function() {
+      var React = require('react/addons');
+      var TestUtils = React.addons.TestUtils;
+      var repairButton = TestUtils.renderIntoDocument(
+        <RepairButton/>
+      );
+      var button = TestUtils.findRenderedDOMComponentWithTag(repairButton, 'button');
+      TestUtils.Simulate.click(button);
+      expect(FeedbackDialogs.machineIssue).toBeCalled();
+    });
+  });
+});
