8474e738bbbdde7e90e60708eb360f777367a0b9
Philip Silva <psilva.de@gmail.com>
2015-09-18 11:44:03 +0200

    Migrations: changed ordering of autoextend migrations, so they are not skipped

--- database/migrations/20150910_110015_usermembershipautoextend.go
+++ database/migrations/20150910_110015_usermembershipautoextend.go
@@ -5,23 +5,23 @@ import (
 )
 
 // DO NOT MODIFY
-type Usermembershipautoextend_20150909_110015 struct {
+type Usermembershipautoextend_20150910_110015 struct {
 	migration.Migration
 }
 
 // DO NOT MODIFY
 func init() {
-	m := &Usermembershipautoextend_20150909_110015{}
-	m.Created = "20150909_110015"
-	migration.Register("Usermembershipautoextend_20150909_110015", m)
+	m := &Usermembershipautoextend_20150910_110015{}
+	m.Created = "20150910_110015"
+	migration.Register("Usermembershipautoextend_20150910_110015", m)
 }
 
 // Run the migrations
-func (m *Usermembershipautoextend_20150909_110015) Up() {
+func (m *Usermembershipautoextend_20150910_110015) Up() {
 	m.Sql("ALTER TABLE user_membership ADD COLUMN auto_extend TINYINT(1)")
 }
 
 // Reverse the migrations
-func (m *Usermembershipautoextend_20150909_110015) Down() {
+func (m *Usermembershipautoextend_20150910_110015) Down() {
 	m.Sql("ALTER TABLE user_membership DROP COLUMN auto_extend")
 }
--- database/migrations/20150910_172301_autoextendmemberships.go
+++ database/migrations/20150910_172301_autoextendmemberships.go
@@ -5,25 +5,25 @@ import (
 )
 
 // DO NOT MODIFY
-type Autoextendmemberships_20150908_172301 struct {
+type Autoextendmemberships_20150910_172301 struct {
 	migration.Migration
 }
 
 // DO NOT MODIFY
 func init() {
-	m := &Autoextendmemberships_20150908_172301{}
-	m.Created = "20150908_172301"
-	migration.Register("Autoextendmemberships_20150908_172301", m)
+	m := &Autoextendmemberships_20150910_172301{}
+	m.Created = "20150910_172301"
+	migration.Register("Autoextendmemberships_20150910_172301", m)
 }
 
 // Run the migrations
-func (m *Autoextendmemberships_20150908_172301) Up() {
+func (m *Autoextendmemberships_20150910_172301) Up() {
 	m.Sql("ALTER TABLE membership ADD COLUMN auto_extend TINYINT(1)")
 	m.Sql("ALTER TABLE membership ADD COLUMN auto_extend_duration INT(11)")
 }
 
 // Reverse the migrations
-func (m *Autoextendmemberships_20150908_172301) Down() {
+func (m *Autoextendmemberships_20150910_172301) Down() {
 	m.Sql("ALTER TABLE membership DROP COLUMN auto_extend")
 	m.Sql("ALTER TABLE membership DROP COLUMN auto_extend_duration")
 }
