8cf1cfcef1cce3b7937eb853f4e3650f04b56aab
Philip Silva <psilva.de@gmail.com>
2015-09-24 12:12:24 +0200

    Migration: set auto extend to true for all memberships and user memberships; auto extend duration = 1

---
+++ database/migrations/20150924_120425_autoextenddefaulttrue.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/astaxie/beego/migration"
+)
+
+// DO NOT MODIFY
+type Autoextenddefaulttrue_20150924_120425 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &Autoextenddefaulttrue_20150924_120425{}
+	m.Created = "20150924_120425"
+	migration.Register("Autoextenddefaulttrue_20150924_120425", m)
+}
+
+// Run the migrations
+func (m *Autoextenddefaulttrue_20150924_120425) Up() {
+	m.Sql("UPDATE membership SET auto_extend = TRUE, auto_extend_duration_months = 1")
+	m.Sql("UPDATE user_membership SET auto_extend = TRUE")
+}
+
+// Reverse the migrations
+func (m *Autoextenddefaulttrue_20150924_120425) Down() {
+	m.Sql("UPDATE membership SET auto_extend = NULL, auto_extend_duration_months = NULL")
+	m.Sql("UPDATE user_membership SET auto_extend = NULL")
+}
