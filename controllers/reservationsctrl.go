390804a90127276e6e4eb46b550d37d4473cdb1b
Philip Silva <psilva.de@gmail.com>
2015-09-28 18:23:42 +0200

    Reservations: first simple API

---
+++ controllers/reservationsctrl.go
@@ -0,0 +1,104 @@
+package controllers
+
+import (
+	"encoding/json"
+	"github.com/astaxie/beego"
+	"github.com/kr15h/fabsmith/models"
+)
+
+type ReservationsController struct {
+	Controller
+}
+
+// @Title GetAll
+// @Description Get all reservations
+// @Success 200 {object} models.Reservation
+// @Failure	403	Failed to get all reservations
+// @Failure	401 Not authorized
+// @router / [get]
+func (this *ReservationsController) GetAll() {
+	reservations, err := models.GetAllReservations()
+	if err != nil {
+		this.CustomAbort(403, "Failed to get all reservations")
+	}
+	this.Data["json"] = reservations
+	this.ServeJson()
+}
+
+// @Title Get
+// @Description Get reservation by ID
+// @Param	mid		path 	int	true		"Reservation ID"
+// @Success 200 {object} models.Reservation
+// @Failure	403	Failed to get reservation
+// @Failure	401	Not authorized
+// @router /:rid [get]
+func (this *ReservationsController) Get() {
+	rid, err := this.GetInt64(":rid")
+	if err != nil {
+		beego.Error("Failed to get :rid variable")
+		this.CustomAbort(403, "Failed to get reservation")
+	}
+
+	reservation, err := models.GetReservation(rid)
+	if err != nil {
+		beego.Error("Failed to get reservation", err)
+		this.CustomAbort(403, "Failed to get reservation")
+	}
+
+	this.Data["json"] = reservation
+	this.ServeJson()
+}
+
+// @Title Create
+// @Description Create reservation
+// @Param	model	body	models.Reservation	true	"Reservation Name"
+// @Success 200 {object} models.ReservationCreatedResponse
+// @Failure	403	Failed to create reservation
+// @Failure	401	Not authorized
+// @router / [post]
+func (this *ReservationsController) Create() {
+	dec := json.NewDecoder(this.Ctx.Request.Body)
+	req := models.Reservation{}
+	if err := dec.Decode(&req); err != nil {
+		beego.Error("Failed to decode json:", err)
+		this.CustomAbort(403, "Failed to create reservation")
+	}
+
+	id, err := models.CreateReservation(&req)
+	if err != nil {
+		beego.Error("Failed to create reservation", err)
+		this.CustomAbort(403, "Failed to create reservation")
+	}
+
+	this.Data["json"] = map[string]int64{"Id": id}
+	this.ServeJson()
+}
+
+// @Title Delete
+// @Description Delete reservation
+// @Param	rid	path	int	true	"Reservation ID"
+// @Success 200 string ok
+// @Failure	403	Failed to delete reservation
+// @Failure	401	Not authorized
+// @router /:rid [delete]
+func (this *ReservationsController) Delete() {
+	if !this.IsAdmin() {
+		beego.Error("Not authorized")
+		this.CustomAbort(401, "Not authorized")
+	}
+
+	rid, err := this.GetInt64(":rid")
+	if err != nil {
+		beego.Error("Failed to get rid:", err)
+		this.CustomAbort(403, "Failed to delete reservation")
+	}
+
+	err = models.DeleteReservation(rid)
+	if err != nil {
+		beego.Error("Failed to delete reservation:", err)
+		this.CustomAbort(403, "Failed to delete reservation")
+	}
+
+	this.Data["json"] = "ok"
+	this.ServeJson()
+}
---
+++ database/migrations/20150928_181415_reservations.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/astaxie/beego/migration"
+)
+
+// DO NOT MODIFY
+type Reservations_20150928_181415 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &Reservations_20150928_181415{}
+	m.Created = "20150928_181415"
+	migration.Register("Reservations_20150928_181415", m)
+}
+
+// Run the migrations
+func (m *Reservations_20150928_181415) Up() {
+	m.Sql(`CREATE TABLE reservations (
+		id int(11) unsigned NOT NULL AUTO_INCREMENT,
+		machine_id int(11) NOT NULL,
+		user_id int(11) NOT NULL,
+		time_start datetime NOT NULL,
+		time_end datetime NOT NULL,
+		created datetime NOT NULL,
+		PRIMARY KEY (id)
+	)`)
+}
+
+// Reverse the migrations
+func (m *Reservations_20150928_181415) Down() {
+	m.Sql("DROP TABLE reservations")
+}
