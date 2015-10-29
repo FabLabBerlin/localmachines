4b189c5a7a05b828ba93bf7c9d42e8cfff174a3c
Philip Silva <psilva.de@gmail.com>
2015-10-29 14:41:14 +0100

    Admin Dashboard += first graph with metrics

--- clients/admin/dev/ng-modules/dashboard/dashboard.js
+++ clients/admin/dev/ng-modules/dashboard/dashboard.js
@@ -14,8 +14,69 @@ app.config(['$routeProvider', function($routeProvider) {
 app.controller('DashboardCtrl', ['$scope', '$http', '$location', 
  function($scope, $http, $location) {
 
- 	// Do stuff here
-	
+  $scope.metrics = [];
+
+  $scope.loadMetricsData = function() {
+    $http({
+      method: 'GET',
+      url: '/api/metrics',
+      params: {
+        ac: new Date().getTime()
+      }
+    })
+    .success(function(metrics) {
+      $scope.metrics = metrics;
+      $scope.renderChartsInit();
+    })
+    .error(function(data, status) {
+      toastr.error('Failed to load metrics data');
+    });
+  };
+
+  $scope.renderCharts = function(a, b) {
+    var months = _.map($scope.metrics.ActivationsByMonth, function(sum, month) {
+      return month;
+    }).sort();
+    var byMonth = months.map(function(month) {
+      return [
+        {
+          v: month,
+          f: month
+        },
+        Math.round($scope.metrics.ActivationsByMonth[month]),
+        Math.round($scope.metrics.MembershipsByMonth[month])
+      ];
+    });
+
+
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Time of Day');
+    data.addColumn('number', 'Activations (€)');
+    data.addColumn('number', 'Memberships (€)');
+    data.addRows(byMonth);
+
+    var options = {
+      title: 'Revenue through Activations and Memberships',
+      hAxis: {
+        title: 'Month',
+      },
+      vAxis: {
+        title: 'Revenue / €'
+      }
+    };
+
+    var chart = new google.visualization.ColumnChart(
+      document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+  };
+
+  $scope.renderChartsInit = function() {
+    $scope.renderCharts();
+  };
+
+  $scope.loadMetricsData();
+
 }]); // app.controller
 
 })(); // closure
