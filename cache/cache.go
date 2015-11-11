95fda5112241419e908af2cbdc53279b609a4cb7
Philip Silva <psilva.de@gmail.com>
2015-11-11 17:26:47 +0100

    Cache Server: able to allow users to (de)activate machines

--- cache/cache.go
+++ cache/cache.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"html"
 	"io"
 	"log"
 	"net/http"
-	"net/url"
 )
+
+var masterHost *string
 
 func isSafe(path string) bool {
 	return true
@@ -21,39 +23,42 @@ func forwardHeader(w http.ResponseWriter, resp *http.Response, key string) {
 	}
 }
 
+func newRequest(browserReq *http.Request) (req *http.Request, err error) {
+	var body io.Reader
+	if browserReq.Method != "GET" {
+		buf := new(bytes.Buffer)
+		if _, err := io.Copy(buf, browserReq.Body); err != nil {
+			return nil, fmt.Errorf("io.Copy r.Body: %v", err)
+		}
+		body = buf
+	}
+	uri := "http://" + *masterHost + browserReq.URL.Path
+	req, err = http.NewRequest(browserReq.Method, uri, body)
+	if err != nil {
+		return nil, fmt.Errorf("http.NewRequest: %v", err)
+	}
+	for _, cookie := range browserReq.Cookies() {
+		req.AddCookie(cookie)
+	}
+	if len(browserReq.Header["Content-Type"]) == 1 {
+		req.Header.Set("Content-Type", browserReq.Header["Content-Type"][0])
+	}
+	return
+}
+
 func main() {
-	masterHost := flag.String("master", "127.0.0.1:8080", "Master host")
+	masterHost = flag.String("master", "127.0.0.1:8080", "Master host")
 	listenHost := flag.String("listen", "127.0.0.1:1234", "Listen host")
 	flag.Parse()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/" {
 			http.Redirect(w, r, "machines/", 302)
-		} else if r.URL.Path == "/api/users/login" {
-			form := url.Values{
-				"username": {r.FormValue("username")},
-				"password": {r.FormValue("password")},
-			}
-			resp, err := http.PostForm("http://"+*masterHost+"/api/users/login", form)
-			if err != nil {
-				log.Fatalf("http.Get: %v", err)
-			}
-			defer resp.Body.Close()
-			forwardHeader(w, resp, "Content-Type")
-			forwardHeader(w, resp, "Cookie")
-			forwardHeader(w, resp, "Set-Cookie")
-			_, err = io.Copy(w, resp.Body)
-			if err != nil {
-				log.Fatalf("io.Copy: %v", err)
-			}
 		} else if isSafe(r.URL.Path) {
 			client := &http.Client{}
-			req, err := http.NewRequest("GET", "http://"+*masterHost+r.URL.Path, nil)
+			req, err := newRequest(r)
 			if err != nil {
 				log.Fatalf("http.NewRequest: %v", err)
-			}
-			for _, cookie := range r.Cookies() {
-				req.AddCookie(cookie)
 			}
 			resp, err := client.Do(req)
 			if err != nil {
@@ -65,7 +70,7 @@ func main() {
 			forwardHeader(w, resp, "Set-Cookie")
 			_, err = io.Copy(w, resp.Body)
 			if err != nil {
-				log.Fatalf("io.Copy: %v", err)
+				log.Fatalf("io.Copy resp.Body: %v", err)
 			}
 		} else {
 			fmt.Fprintf(w, "Sorry not possible. Thank you.")
