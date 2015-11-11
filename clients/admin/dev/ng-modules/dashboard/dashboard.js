8b8a4a7eadd650bc8577a6c1240093fd36dc734e
Philip Silva <psilva.de@gmail.com>
2015-11-11 11:48:25 +0100

    chart js += 'legend template'

--- clients/admin/dev/ng-modules/dashboard/dashboard.js
+++ clients/admin/dev/ng-modules/dashboard/dashboard.js
@@ -80,22 +80,6 @@ app.controller('DashboardCtrl', ['$scope', '$http', '$location',
             return Math.round($scope.metrics.MembershipsByMonth[month]);
           })
         }
-        /*{
-          label: "My First dataset",
-          fillColor: "rgba(220,220,220,0.5)",
-          strokeColor: "rgba(220,220,220,0.8)",
-          highlightFill: "rgba(220,220,220,0.75)",
-          highlightStroke: "rgba(220,220,220,1)",
-          data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-          label: "My Second dataset",
-          fillColor: "rgba(151,187,205,0.5)",
-          strokeColor: "rgba(151,187,205,0.8)",
-          highlightFill: "rgba(151,187,205,0.75)",
-          highlightStroke: "rgba(151,187,205,1)",
-          data: [28, 48, 40, 19, 86, 27, 90]
-        }*/
       ]
     };
 
@@ -106,7 +90,8 @@ app.controller('DashboardCtrl', ['$scope', '$http', '$location',
       },
       vAxis: {
         title: 'Revenue / €'
-      }
+      },
+      legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
     };
 
     var ctx = document.getElementById('chart_monthly').getContext('2d');
