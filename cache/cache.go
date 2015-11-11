b2aec491b99857f8971dcbc665944d3faf5b5cb4
Philip Silva <psilva.de@gmail.com>
2015-11-11 16:54:53 +0100

    Cache Server: able to login and the machines status

--- cache/cache.go
+++ cache/cache.go
@@ -7,10 +7,18 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func isSafe(path string) bool {
 	return true
+}
+
+func forwardHeader(w http.ResponseWriter, resp *http.Response, key string) {
+	value := resp.Header[key]
+	if len(value) == 1 {
+		w.Header().Set(key, value[0])
+	}
 }
 
 func main() {
@@ -21,15 +29,40 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/" {
 			http.Redirect(w, r, "machines/", 302)
-		} else if isSafe(r.URL.Path) {
-			resp, err := http.Get("http://" + *masterHost + r.URL.Path)
+		} else if r.URL.Path == "/api/users/login" {
+			form := url.Values{
+				"username": {r.FormValue("username")},
+				"password": {r.FormValue("password")},
+			}
+			resp, err := http.PostForm("http://"+*masterHost+"/api/users/login", form)
 			if err != nil {
 				log.Fatalf("http.Get: %v", err)
 			}
-			contentType := resp.Header["Content-Type"]
-			if len(contentType) == 1 {
-				w.Header().Set("Content-Type", contentType[0])
+			defer resp.Body.Close()
+			forwardHeader(w, resp, "Content-Type")
+			forwardHeader(w, resp, "Cookie")
+			forwardHeader(w, resp, "Set-Cookie")
+			_, err = io.Copy(w, resp.Body)
+			if err != nil {
+				log.Fatalf("io.Copy: %v", err)
 			}
+		} else if isSafe(r.URL.Path) {
+			client := &http.Client{}
+			req, err := http.NewRequest("GET", "http://"+*masterHost+r.URL.Path, nil)
+			if err != nil {
+				log.Fatalf("http.NewRequest: %v", err)
+			}
+			for _, cookie := range r.Cookies() {
+				req.AddCookie(cookie)
+			}
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Fatalf("http.Get: %v", err)
+			}
+			defer resp.Body.Close()
+			forwardHeader(w, resp, "Content-Type")
+			forwardHeader(w, resp, "Cookie")
+			forwardHeader(w, resp, "Set-Cookie")
 			_, err = io.Copy(w, resp.Body)
 			if err != nil {
 				log.Fatalf("io.Copy: %v", err)
