b3fdcd0cb0d3596a63f0ee032a9f0da907a278d4
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-11-18 12:07:39 +0100

    Admin: Make use of global settings in tutoring overview

--- clients/admin/dev/ng-modules/tutoring/tutoring.js
+++ clients/admin/dev/ng-modules/tutoring/tutoring.js
@@ -20,9 +20,41 @@ app.controller('TutoringCtrl', ['$scope', '$http', '$location',
   ];
 
   $scope.purchases = [
-    {Id:1, TutorName:'Ahmad Taleb', UserName:'Selma Atari', StartTime:'15 Oct 2015 12:00', EndTime:'15 Oct 2015 14:00', TotalTime:1, TotalPrice:120, VAT:22.8},
-    {Id:1, TutorName:'Ahmad Taleb', UserName:'Selma Atari', StartTime:'15 Oct 2015 16:00', EndTime:'15 Oct 2015 18:00', TotalTime:1, TotalPrice:120, VAT:22.8}
+    {Id:1, TutorName:'Ahmad Taleb', UserName:'Selma Atari', StartTime:'15 Oct 2015 12:00', EndTime:'15 Oct 2015 14:00', TotalTime:1, TotalPrice:120, VAT:0},
+    {Id:1, TutorName:'Ahmad Taleb', UserName:'Selma Atari', StartTime:'15 Oct 2015 16:00', EndTime:'15 Oct 2015 18:00', TotalTime:1, TotalPrice:120, VAT:0}
   ];
+
+  // Load global settings for the VAT and currency
+  $scope.loadSettings = function() {
+    $http({
+      method: 'GET',
+      url: '/api/settings',
+      params: {
+        ac: new Date().getTime()
+      }
+    })
+    .success(function(settings) {
+      $scope.settings = {
+        Currency: {},
+        VAT: {}
+      };
+      console.log(settings);
+      _.each(settings, function(setting) {
+        $scope.settings[setting.Name] = setting;
+      });
+      $scope.updateLists();
+    })
+    .error(function() {
+      toastr.error('Failed to get global config');
+    });
+  };
+
+  $scope.updateLists = function() {
+    // Update VAT calculation
+    _.each($scope.purchases, function(purchase) {
+      purchase.VAT = $scope.settings.VAT.ValueFloat * purchase.TotalPrice / 100.0;
+    });
+  }
 
   $scope.addTutor = function() {
     alert('Add tutor');
@@ -31,6 +63,8 @@ app.controller('TutoringCtrl', ['$scope', '$http', '$location',
   $scope.addPurchase = function() {
     alert('Add purchase');
   }
+
+  $scope.loadSettings();
 
 }]); // app.controller
 
