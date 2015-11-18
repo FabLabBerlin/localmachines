bb1782b8d9a78ce7687c8e05488d633243ccf074
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-11-18 15:07:57 +0100

    Admin: Tutoring: Remove obsolete VAT calculation before purchases are loaded

--- clients/admin/dev/ng-modules/tutoring/tutoring.js
+++ clients/admin/dev/ng-modules/tutoring/tutoring.js
@@ -14,21 +14,6 @@ app.config(['$routeProvider', function($routeProvider) {
 app.controller('TutoringCtrl', ['$scope', '$http', '$location', 
   function($scope, $http, $location) {
 
-  $scope.tutors = [
-    {Id:1, Name:'Ahmad Taleb', Price:60, PriceUnit:'hour'},
-    {Id:2, Name:'Zelma Atari', Price:60, PriceUnit:'hour'}
-  ];
-
-  $scope.purchases = [
-    {Id:1, TutorName:'Ahmad Taleb', UserName:'Selma Atari', StartTime:'15 Oct 2015 12:00', EndTime:'15 Oct 2015 14:00', TotalTime:1, TotalPrice:120, VAT:19},
-    {Id:1, TutorName:'Ahmad Taleb', UserName:'Selma Atari', StartTime:'15 Oct 2015 16:00', EndTime:'15 Oct 2015 18:00', TotalTime:1, TotalPrice:120, VAT:19}
-  ];
-
-  // Calculate the TotalPriceVAT
-  _.each($scope.purchases, function(purchase) {
-    purchase.TotalPriceVAT = purchase.VAT * purchase.TotalPrice / 100.0;
-  });
-
   // Load global settings for the VAT and currency
   $scope.loadSettings = function() {
     $http({
