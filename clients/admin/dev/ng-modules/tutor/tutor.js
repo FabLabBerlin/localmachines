95772d08142095d1ed99119a376b7a8bbc052e7a
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-11-18 16:31:50 +0100

    Admin: Tutor: Connect tutor view with backend API

--- clients/admin/dev/ng-modules/tutor/tutor.js
+++ clients/admin/dev/ng-modules/tutor/tutor.js
@@ -14,7 +14,53 @@ app.config(['$routeProvider', function($routeProvider) {
 app.controller('TutorCtrl', ['$scope', '$http', '$location', 
   function($scope, $http, $location) {
 
-  
+  $scope.tutor = {
+    PriceUnit: 'hour'
+  };
+
+  $scope.cancel = function() {
+    $location.path('/tutoring');
+  };
+
+  $scope.save = function() {
+
+    if ($scope.tutor.Name === '' || !$scope.tutor.Name) {
+      alert('Enter tutor name');
+      return;
+    }
+
+    if ($scope.tutor.Price === '' || !$scope.tutor.Price) {
+      alert('Enter tutor price');
+      return;
+    }
+
+    console.log($scope.tutor);
+
+    var tutor = $scope.tutor;
+    tutor.Price = parseInt(tutor.Price);
+
+    $http({
+      method: 'PUT',
+      url: '/api/tutoring/tutor',
+      data: tutor,
+      headers: { 
+        'Content-Type': 'application/json' 
+      },
+      params: { 
+        ac: new Date().getTime() 
+      },
+      transformRequest: function(data) {
+        return JSON.stringify(data);
+      }
+    })
+    .success(function(data) {
+      toastr.success('Tutor updated');
+      $location.path('/tutoring');
+    })
+    .error(function() {
+      toastr.error('Failed to save tutor');
+    });
+  };
 
 }]); // app.controller
 
