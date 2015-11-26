5881c6937b6626f531b3dfd70662721d959b0791
Philip Silva <psilva.de@gmail.com>
2015-11-26 09:29:49 +0100

    machines: don't show debug messages on host lab.fablab.berlin

--- clients/machines/src/js/reactor.js
+++ clients/machines/src/js/reactor.js
@@ -2,5 +2,5 @@ var Nuclear = require('nuclear-js');
 
 
 export default new Nuclear.Reactor({
-  debug: window.location.host !== 'easylab.io'
+  debug: window.location.host !== 'easylab.io' && window.location.host !== 'lab.fablab.berlin'
 });
