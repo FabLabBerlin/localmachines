766a7b53bc2988976adb590f95eee6dc24652b1f
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-12-02 17:48:48 +0100

    Add `archived` column of type TINYINT to the products table

---
+++ database/migrations/20151202_172646_productarchived.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/astaxie/beego/migration"
+)
+
+// DO NOT MODIFY
+type Productarchived_20151202_172646 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &Productarchived_20151202_172646{}
+	m.Created = "20151202_172646"
+	migration.Register("Productarchived_20151202_172646", m)
+}
+
+// Run the migrations
+func (m *Productarchived_20151202_172646) Up() {
+	m.Sql("ALTER TABLE products ADD COLUMN archived TINYINT(1) DEFAULT 0")
+}
+
+// Reverse the migrations
+func (m *Productarchived_20151202_172646) Down() {
+	// use m.Sql("DROP TABLE ...") to reverse schema update
+	m.Sql("ALTER TABLE products DROP COLUMN archived")
+}
