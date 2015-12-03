2cf1e4281d9da10900604d006bb557726aeaac34
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-12-03 14:40:09 +0100

    Models: Add `ArchivePurchase` func
    Also add `Archived` field to the `Purchase` type struct

--- models/purchases.go
+++ models/purchases.go
@@ -52,6 +52,8 @@ type Purchase struct {
 	Reservation  *Reservation  `orm:"-"`
 	MachineUsage time.Duration `orm:"-"`
 	Memberships  []*Membership `orm:"-"`
+
+	Archived bool
 }
 
 func (this *Purchase) TableName() string {
@@ -73,6 +75,13 @@ func GetAllPurchasesOfType(purchaseType string) (purchases []*Purchase, err erro
 func DeletePurchase(id int64) (err error) {
 	o := orm.NewOrm()
 	_, err = o.Delete(&Purchase{Id: id})
+	return
+}
+
+func ArchivePurchase(purchase *Purchase) (err error) {
+	o := orm.NewOrm()
+	purchase.Archived = true
+	_, err = o.Update(purchase)
 	return
 }
 
