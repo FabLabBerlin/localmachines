f4778ac438d69b8d50d61a9cafdf2135ec80ff04
Philip Silva <psilva.de@gmail.com>
2015-12-09 14:39:56 +0100

    Model Tests: use model.TableName()

--- tests/setup/database.go
+++ tests/setup/database.go
@@ -10,8 +10,22 @@ import (
 	"github.com/kr15h/fabsmith/database/connect"
 	"github.com/kr15h/fabsmith/models"
 	"github.com/kr15h/fabsmith/models/invoices"
+	"github.com/kr15h/fabsmith/models/products"
 	"github.com/kr15h/fabsmith/models/purchases"
 )
+
+var dbModels = []Model{
+	&invoices.Invoice{},
+	&models.Auth{},
+	&models.Machine{},
+	&models.Membership{},
+	&models.NetSwitchMapping{},
+	&models.Permission{},
+	&models.User{},
+	&products.Product{},
+	&purchases.Purchase{},
+	&models.UserMembership{},
+}
 
 // ConfigDB : Configure database for tests
 func ConfigDB() {
@@ -52,64 +66,18 @@ func ConfigDB() {
 	connect.Connect(mysqlUser, mysqlPass, mysqlHost, mysqlPort, mysqlDb)
 }
 
+type Model interface {
+	TableName() string
+}
+
 // ResetDB : Reset the database after each test
 func ResetDB() {
 	o := orm.NewOrm()
 
-	// TODO: Use model.TableName() for table names
-
-	var machines []models.Machine
-	o.QueryTable("machines").All(&machines)
-	for _, item := range machines {
-		o.Delete(&item)
+	for _, dbModel := range dbModels {
+		query := fmt.Sprintf("DELETE FROM %v", dbModel.TableName())
+		if _, err := o.Raw(query).Exec(); err != nil {
+			panic(err.Error())
+		}
 	}
-
-	var memberships []models.Membership
-	o.QueryTable("membership").All(&memberships)
-	for _, item := range memberships {
-		o.Delete(&item)
-	}
-
-	var invoices []invoices.Invoice
-	o.QueryTable("invoices").All(&invoices)
-	for _, item := range invoices {
-		o.Delete(&item)
-	}
-
-	var netswitches []models.NetSwitchMapping
-	o.QueryTable("netswitch").All(&netswitches)
-	for _, item := range netswitches {
-		o.Delete(&item)
-	}
-
-	var users []models.User
-	o.QueryTable("user").All(&users)
-	for _, item := range users {
-		o.Delete(&item)
-	}
-
-	var user_memberships []models.UserMembership
-	o.QueryTable("user_membership").All(&user_memberships)
-	for _, item := range user_memberships {
-		o.Delete(&item)
-	}
-
-	var permissions []models.Permission
-	o.QueryTable("permission").All(&permissions)
-	for _, item := range permissions {
-		o.Delete(&item)
-	}
-
-	var auths []models.Auth
-	o.QueryTable("auth").All(&auths)
-	for _, item := range auths {
-		o.Delete(&item)
-	}
-
-	var purchases []purchases.Purchase
-	o.QueryTable("purchases").All(&purchases)
-	for _, item := range purchases {
-		o.Delete(&item)
-	}
-
 }
