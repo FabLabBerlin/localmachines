0c7be91f3b6d2253122ffbf248f3ef70a8c23a0f
Philip Silva <psilva.de@gmail.com>
2015-12-10 16:34:19 +0100

    Tests for co-working and space products

---
+++ tests/models/products/coworking_test.go
@@ -0,0 +1,46 @@
+package products
+
+import (
+	"github.com/kr15h/fabsmith/models/products"
+	"github.com/kr15h/fabsmith/tests/assert"
+	"github.com/kr15h/fabsmith/tests/setup"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func init() {
+	setup.ConfigDB()
+}
+
+func TestCoWorkingPurchases(t *testing.T) {
+	Convey("Testing CoWorking purchase model", t, func() {
+
+		Reset(setup.ResetDB)
+
+		Convey("CreateCoWorking and GetCoWorking", func() {
+			c, err := products.CreateCoWorking("foo")
+			c2, err2 := products.GetCoWorking(c.Product.Id)
+			assert.NoErrors(err, err2)
+			So(c2.Product.Name, ShouldEqual, "foo")
+		})
+
+		Convey("GetAllCoWorkings", func() {
+			c1, err1 := products.CreateCoWorking("foo")
+			c2, err2 := products.CreateCoWorking("bar")
+			cs, err := products.GetAllCoWorking()
+			assert.NoErrors(err1, err2, err)
+			So(len(cs), ShouldEqual, 2)
+			So(c1.Product.Id, ShouldEqual, cs[0].Product.Id)
+			So(c2.Product.Id, ShouldEqual, cs[1].Product.Id)
+		})
+
+		Convey("Update", func() {
+			c, err := products.CreateCoWorking("foobar")
+			c.Product.Name = "foobaz"
+			err2 := c.Update()
+			cc, err3 := products.GetCoWorking(c.Product.Id)
+			assert.NoErrors(err, err2, err3)
+			So("foobaz", ShouldEqual, cc.Product.Name)
+		})
+	})
+}
