24b44defcf2eea82cd18e66454ba6b9cf8b003fb
Philip Silva <psilva.de@gmail.com>
2016-01-05 16:06:26 +0100

    gateway: save on/off state to json

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -8,7 +8,8 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
-	//"strings"
+	"os"
+	"os/signal"
 )
 
 type LoginResp struct {
@@ -20,7 +21,7 @@ func (resp *LoginResp) ok() bool {
 	return resp.Status == "ok"
 }
 
-var netSwitches []NetSwitch
+var netSwitches map[int64]*NetSwitch
 
 type NetSwitch struct {
 	Id        int64
@@ -56,14 +57,58 @@ func Fetch(client *http.Client, apiUrl string) (err error) {
 	}
 	defer resp.Body.Close()
 	dec := json.NewDecoder(resp.Body)
-	if err := dec.Decode(&netSwitches); err != nil {
+	nss := []NetSwitch{}
+	if err := dec.Decode(&nss); err != nil {
 		return fmt.Errorf("json decode: %v", err)
 	}
-	log.Printf("netswitches: %v", netSwitches)
+	log.Printf("netswitches: %v", nss)
+	netSwitches = make(map[int64]*NetSwitch)
+	for _, ns := range nss {
+		netSwitches[ns.MachineId] = &ns
+	}
 	return
 }
 
-func Init(apiUrl, user, key string) (err error) {
+func LoadState(filename string) (err error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return
+	}
+	defer f.Close()
+	dec := json.NewDecoder(f)
+	var switchStates []NetSwitch
+	if err := dec.Decode(&switchStates); err != nil {
+		return fmt.Errorf("json decode: %v", err)
+	}
+	for _, switchState := range switchStates {
+		mid := switchState.MachineId
+		if netswitch, ok := netSwitches[mid]; ok {
+			netswitch.On = switchState.On
+		} else {
+			log.Printf("netswitch for machine id %v doesn't exist anymore", mid)
+		}
+	}
+	return
+}
+
+func SaveState(filename string) (err error) {
+	f, err := os.Create(filename)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	enc := json.NewEncoder(f)
+	switchStates := make([]NetSwitch, 0, len(netSwitches))
+	for _, ns := range netSwitches {
+		switchStates = append(switchStates, *ns)
+	}
+	return enc.Encode(switchStates)
+}
+
+func Init(apiUrl, user, key, stateFile string) (err error) {
 
 	client := &http.Client{}
 	if client.Jar, err = cookiejar.New(nil); err != nil {
@@ -75,6 +120,22 @@ func Init(apiUrl, user, key string) (err error) {
 	if err := Fetch(client, apiUrl); err != nil {
 		return fmt.Errorf("fetch: %v", err)
 	}
+	if err := LoadState(stateFile); err != nil {
+		return fmt.Errorf("load state: %v", err)
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			log.Printf("received signal %v", sig)
+			// sig is a ^C, handle it
+			if err := SaveState(stateFile); err != nil {
+				log.Printf("failed saving state: %v", err)
+			}
+			os.Exit(1)
+		}
+	}()
 
 	return
 }
@@ -83,8 +144,12 @@ func main() {
 	apiUrl := flag.String("apiUrl", "http://localhost:8080/api", "Url of the fabsmith api (http or https)")
 	user := flag.String("id", "user", "id")
 	key := flag.String("key", "user", "key")
+	stateFile := flag.String("stateFile", "state.json", "switches are stateful but they loose state on reset")
 	flag.Parse()
-	if err := Init(*apiUrl, *user, *key); err != nil {
+	if err := Init(*apiUrl, *user, *key, *stateFile); err != nil {
 		log.Fatalf("Init: %v", err)
+	}
+
+	for true {
 	}
 }
