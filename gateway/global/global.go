d5e9a581a09af10883eb3e24fb2a74e162200ca7
Philip Silva <psilva.de@gmail.com>
2016-01-08 16:46:19 +0100

    xmpp: use safe parameters

--- gateway/global/global.go
+++ gateway/global/global.go
@@ -8,8 +8,8 @@ const (
 	STATE_SYNC_TIMEOUT            = 9 * time.Second
 	STATE_SYNC_PERIOD             = 3 * STATE_SYNC_TIMEOUT
 	XMPP_DEBUG                    = false
-	XMPP_NO_TLS                   = true
-	XMPP_TLS_INSECURE_SKIP_VERIFY = true
+	XMPP_NO_TLS                   = false
+	XMPP_TLS_INSECURE_SKIP_VERIFY = false
 	XMPP_USE_SERVER_SESSION       = false
 )
 
