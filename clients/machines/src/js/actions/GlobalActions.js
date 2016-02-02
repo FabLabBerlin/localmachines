668c88069be19cfbf9312f26c4b73f0184872bae
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-02-02 15:10:03 +0100

    Clean up `GlobalActions`

--- clients/machines/src/js/actions/GlobalActions.js
+++ clients/machines/src/js/actions/GlobalActions.js
@@ -1,6 +1,5 @@
 var $ = require('jquery');
 var actionTypes = require('../actionTypes');
-var reactor = require('../reactor');
 
 function showGlobalLoader() {
   reactor.dispatch(actionTypes.SET_LOADING);
@@ -10,19 +9,6 @@ function hideGlobalLoader() {
   reactor.dispatch(actionTypes.UNSET_LOADING);
 }
 
-function loadAvailableLocations() {
-  $.ajax({
-    url: '/api/locations',
-    success(locations) {
-      reactor.dispatch(actionTypes.SET_LOCATIONS, { locations });
-    },
-    error(xhr, status, err) {
-      toastr.error('Error loading locations');
-      console.error(status, err);
-    }
-  });
-}
-
 export default {
-  showGlobalLoader, hideGlobalLoader, loadAvailableLocations
+  showGlobalLoader, hideGlobalLoader
 };
