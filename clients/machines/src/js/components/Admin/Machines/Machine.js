db1923ad81fdb581d9c0c103cdd3d72377001148
Philip Silva <philip@fablab.berlin>
2016-05-10 18:52:21 +0200

    Machine edit: more updates

--- clients/machines/src/js/components/Admin/Machines/Machine.js
+++ clients/machines/src/js/components/Admin/Machines/Machine.js
@@ -92,8 +92,8 @@ var SecondRow = React.createClass({
     var machineImageNewFileName;
     var machineImageNewFileSize;
 
-    if (machine.Image) {
-      machineImageFile = '/files/' + machine.Image;
+    if (machine.get('Image')) {
+      machineImageFile = '/files/' + machine.get('Image');
     }
 
     return (
@@ -116,19 +116,22 @@ var SecondRow = React.createClass({
             <div className="col-sm-6">
               <div className="form-group">
                 <img id="machine-image"
-                  src="{machineImageNewFile || machineImageFile || 'assets/img/img-machine-placeholder.svg'}"
+                  src={machineImageNewFile || machineImageFile || 'assets/img/img-machine-placeholder.svg'}
                   alt="Machine image"/>
               </div>
             </div>
             <div className="col-sm-6">
               <div className="form-group">
-                <input type="file" onchange="angular.element(this).scope().machineImageLoad(this)"/>
-                <button className="btn btn-primary btn-block" ng-disabled="!machineImageNewFile" ng-click="machineImageReplace()">
-                  <i className="fa fa-file-image-o"></i>&nbsp;Replace
+                <input type="file"
+                       onchange="angular.element(this).scope().machineImageLoad(this)"/>
+                <button className="btn btn-primary btn-block"
+                        ng-disabled="!machineImageNewFile"
+                        ng-click="machineImageReplace()">
+                  <i className="fa fa-file-image-o"/>&nbsp;Replace
                 </button>
               </div>
               <p>Supported types: svg</p>
-              <p>Name: {machineImageNewFileName || machine.ImageName}<br/>Size: {machineImageNewFileSize || machine.ImageSize}</p>
+              <p>Name: {machineImageNewFileName || machine.get('ImageName')}<br/>Size: {machineImageNewFileSize || machine.get('ImageSize')}</p>
             </div>
           </div>
 
@@ -247,6 +250,10 @@ var MachineProperties = React.createClass({
 
 
 var NetswitchConfig = React.createClass({
+  applyConfig() {
+    toastr.error('Not implemented here yet');
+  },
+
   render() {
     const machine = this.props.machine;
 
@@ -262,7 +269,7 @@ var NetswitchConfig = React.createClass({
             {machine.get('NetswitchType') ?
               (
                 <div className="col-sm-3">
-                  <div className="form-group" ng-show="machine.NetswitchType">
+                  <div className="form-group">
                     <input type="text"
                            className="form-control"
                            placeholder="Host (mfi only)"
@@ -273,7 +280,7 @@ var NetswitchConfig = React.createClass({
               ) : (
                 <div>
                   <div className="col-sm-3">
-                    <div className="form-group" ng-hide="machine.NetswitchType">
+                    <div className="form-group">
                       <input type="text"
                              className="form-control"
                              placeholder="On URL"
@@ -282,7 +289,7 @@ var NetswitchConfig = React.createClass({
                     </div>
                   </div>
                   <div className="col-sm-3">
-                    <div className="form-group" ng-hide="machine.NetswitchType">
+                    <div className="form-group">
                       <input type="text"
                              className="form-control"
                              placeholder="Off URL"
@@ -313,8 +320,7 @@ var NetswitchConfig = React.createClass({
                 {netswitchConfigStatus ?
                   (
                     <button className="btn btn-danger btn-block"
-                            ng-click="applyConfig()"
-                            ng-disabled="true"
+                            disabled="true"
                             type="button">
                       <i className="fa fa-refresh fa-spin"></i>
                       {netswitchConfigStatus}
@@ -324,7 +330,7 @@ var NetswitchConfig = React.createClass({
                 {(!netswitchConfigStatus && machine.NetswitchType === 'mfi') ?
                   (
                     <button className="btn btn-danger btn-block"
-                            ng-click="applyConfig()"
+                            onClick={this.applyConfig}
                             type="button">
                       Upgrade Powerswitch
                     </button>
