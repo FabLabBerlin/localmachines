8e878cc994900d53f92d2f9081536f53129a07d7
Philip Silva <philip@fablab.berlin>
2016-05-31 18:34:34 +0200

    Fixed fastbill sync upsert

--- models/monthly_earning/invoices/db.go
+++ models/monthly_earning/invoices/db.go
@@ -2,6 +2,7 @@ package invoices
 
 import (
 	"fmt"
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 )
 
@@ -14,20 +15,28 @@ func (inv *Invoice) TableName() string {
 }
 
 // CreateOrUpdate based on LocationId and FastbillId
-func CreateOrUpdate(inv *Invoice) (id int64, err error) {
-	if inv.LocationId == 0 {
+func CreateOrUpdate(invOrig *Invoice) (id int64, err error) {
+	beego.Info("CreateOrUpdate: invOrig.UserId=", invOrig.UserId)
+	if invOrig.LocationId == 0 {
 		return 0, fmt.Errorf("missing location id")
 	}
-	if inv.FastbillId == 0 {
+	if invOrig.FastbillId == 0 {
 		return 0, fmt.Errorf("missing fastbill id")
 	}
 	o := orm.NewOrm()
-	_, id, err = o.ReadOrCreate(inv, "LocationId", "FastbillId")
+	inv := *invOrig
+	created, id, err := o.ReadOrCreate(&inv, "LocationId", "FastbillId")
 	if err != nil {
 		return id, fmt.Errorf("read or create: %v", err)
 	}
+	if inv.UserId == 19 {
+		beego.Info("CreateOrUpdate: created=", created)
+		beego.Info("CreateOrUpdate: id=", id)
+		beego.Info("CreateOrUpdate: inv=", inv)
+	}
+	inv = *invOrig
 	inv.Id = id
-	if _, err = o.Update(inv); err != nil {
+	if _, err = o.Update(&inv); err != nil {
 		return inv.Id, fmt.Errorf("update: %v", err)
 	}
 	return
