ff052bbf1171fdc7afcd89175ccccc87fdcd7d53
Philip Silva <philip@fablab.berlin>
2016-06-07 20:22:35 +0200

    Revert "Special debugging for api user @ loc id 5"
    This reverts commit 3e5b83b2dc5cb00e8189bf96507695204c986d03.

--- controllers/userctrls/usersctrl.go
+++ controllers/userctrls/usersctrl.go
@@ -3,7 +3,6 @@ package userctrls
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/FabLabBerlin/localmachines/controllers"
 	"github.com/FabLabBerlin/localmachines/lib"
 	"github.com/FabLabBerlin/localmachines/models"
@@ -106,9 +105,6 @@ func (this *UsersController) Login() {
 		userId, err := users.AuthenticateUser(username, password)
 		if err != nil {
 			beego.Error("Failed to authenticate @ location = ", locationId)
-			if locationId == 5 && strings.Contains(strings.ToLower(username), "api") {
-				beego.Info("lol123:", fmt.Sprintf("'%v' - '%v'", username, password))
-			}
 			this.CustomAbort(401, "Failed to authenticate")
 		} else {
 			userLocations, err := user_locations.GetAllForUser(userId)
