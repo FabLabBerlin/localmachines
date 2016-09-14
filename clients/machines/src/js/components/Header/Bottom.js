c2231a8edea7b0ac3849f3a7f9f26adf91ec2650
Philip Silva <philip@fablab.berlin>
2016-09-14 16:33:17 +0200

    New nav icons

--- clients/machines/src/js/components/Header/Bottom.js
+++ clients/machines/src/js/components/Header/Bottom.js
@@ -3,18 +3,17 @@ var React = require('react');
 
 var Item = React.createClass({
   render() {
-    var activeClass = '';
-    if (this.props.href === (window.location.pathname + '#' + this.props.location.pathname) ||
+    const isActive = this.props.href === (window.location.pathname + '#' + this.props.location.pathname) ||
       /* TODO: remove, just for ongoing works */
-      this.props.href + 's' === (window.location.pathname + '#' + this.props.location.pathname)) {
-      activeClass = 'active';
-    }
+      this.props.href + 's' === (window.location.pathname + '#' + this.props.location.pathname);
+    const activeClass = isActive ? 'active' : '';
 
     return (
       <div className="nav-item-container">
-        <div className={'nav-item ' + activeClass}>
+        <div className={'nav-item ' + activeClass + ' ' + this.props.className}>
           <a href={this.props.href}>
-            {this.props.label}
+            <span>{this.props.label}</span>
+            <img src={this.props.icon}/>
           </a>
         </div>
       </div>
@@ -27,14 +26,20 @@ var Bottom = React.createClass({
   render() {
     return (
       <div className="nav-bottom row">
-        <Item label={<span><i className="fa fa-plug"/> <span className="hidden-xs">Machines</span></span>}
+        <Item className="nav-item-machines"
               href="/machines/#/machine"
+              icon="/machines/assets/img/header_nav/machine.svg"
+              label="Machines"
               location={this.props.location}/>
-        <Item label={<span><i className="fa fa-calendar-check-o"/> <span className="hidden-xs">Reservations</span></span>}
+        <Item className="nav-item-reservations"
               href="/machines/#/reservations"
+              icon="/machines/assets/img/header_nav/reservations.svg"
+              label="Reservations"
               location={this.props.location}/>
-        <Item label={<span><i className="fa fa-money"/> <span className="hidden-xs">Spendings</span></span>}
+        <Item className="nav-item-spendings"
               href="/machines/#/spendings"
+              icon="/machines/assets/img/header_nav/spendings.svg"
+              label="Spendings"
               location={this.props.location}/>
       </div>
     );
