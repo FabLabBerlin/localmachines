11ef24dbd26bd9a5db2e37017139ed2d7927c621
Philip Silva <philip@fablab.berlin>
2016-10-19 15:27:22 +0200

    Migration rollback improved

--- database/migrations/20161018_174831_purchase_remove_time_end.go
+++ database/migrations/20161018_174831_purchase_remove_time_end.go
@@ -24,8 +24,8 @@ func (m *PurchaseRemoveTimeEnd_20161018_174831) Up() {
 // Reverse the migrations
 func (m *PurchaseRemoveTimeEnd_20161018_174831) Down() {
 	m.SQL("ALTER TABLE purchases ADD COLUMN time_end datetime AFTER time_start")
-	m.SQL("UPDATE purchases SET time_end = date_add(time_start, interval quantity minute) WHERE price_unit = 'minute'")
-	m.SQL("UPDATE purchases SET time_end = date_add(time_start, interval (quantity / 2) hour) WHERE price_unit = '30 minutes'")
-	m.SQL("UPDATE purchases SET time_end = date_add(time_start, interval quantity hour) WHERE price_unit = 'hour'")
-	m.SQL("UPDATE purchases SET time_end = date_add(time_start, interval quantity month) WHERE price_unit = 'month'")
+	m.SQL("UPDATE purchases SET time_end = date_add(time_start, interval quantity minute) WHERE price_unit = 'minute' AND running = 0")
+	m.SQL("UPDATE purchases SET time_end = date_add(time_start, interval (quantity / 2) hour) WHERE price_unit = '30 minutes' AND running = 0")
+	m.SQL("UPDATE purchases SET time_end = date_add(time_start, interval quantity hour) WHERE price_unit = 'hour' AND running = 0")
+	m.SQL("UPDATE purchases SET time_end = date_add(time_start, interval quantity month) WHERE price_unit = 'month' AND running = 0")
 }
