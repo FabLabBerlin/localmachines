ba54e1815081a056f92fdf4914afcc07d8f706f2
Philip Silva <philip@fablab.berlin>
2016-11-07 18:24:31 +0100

    auto extend feature complete

--- models/invoices/invutil/autoextend.go
+++ models/invoices/invutil/autoextend.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/FabLabBerlin/localmachines/models/invoices"
 	"github.com/FabLabBerlin/localmachines/models/locations"
-	"github.com/FabLabBerlin/localmachines/models/user_memberships"
 	"github.com/FabLabBerlin/localmachines/models/users"
 	"time"
 )
@@ -25,7 +24,7 @@ func TaskAutoExtend() (err error) {
 }
 
 func taskAutoExtend(locId int64) (err error) {
-	umsByUid, invsByUid, us, err := taskData(locId)
+	invsByUid, us, err := taskData(locId)
 	if err != nil {
 		return fmt.Errorf("load task data: %v", err)
 	}
@@ -37,71 +36,75 @@ func taskAutoExtend(locId int64) (err error) {
 	}
 
 	for _, u := range us {
-		var currentIv *invoices.Invoice
+		err := taskAutoExtendFor(locId, u, invsByUid, data)
 
-		ivs, ok := invsByUid[u.Id]
-		for _, iv := range ivs {
-			if iv.Month == int(time.Now().Month()) &&
-				iv.Year == time.Now().Year() {
-
-				currentIv = iv
-				break
-			}
-		}
-
-		if currentIv == nil {
-			currentIv, err = invoices.GetDraft(locId, u.Id, time.Now())
-			if err != nil {
-				return fmt.Errorf("get draft: %v", err)
-			}
-		}
-
-		inv := Invoice{Invoice: *currentIv}
-
-		if err = inv.load(*data); err != nil {
-			return fmt.Errorf("load: %v", err)
-		}
-
-		if err := inv.InvoiceUserMemberships(data); err != nil {
-			return fmt.Errorf("invoice user memberships for %v: %v", u.Id, err)
+		if err != nil {
+			return fmt.Errorf("auto extend for user %v: %v", u.Id, err)
 		}
 	}
 
 	return
 }
 
+func taskAutoExtendFor(
+	locId int64,
+	user *users.User,
+	invsByUid map[int64][]*invoices.Invoice,
+	data *PrefetchedData,
+) (err error) {
+	var currentIv *invoices.Invoice
+
+	for _, iv := range invsByUid[user.Id] {
+		if iv.Month == int(time.Now().Month()) &&
+			iv.Year == time.Now().Year() &&
+			iv.Status == "draft" {
+
+			currentIv = iv
+			break
+		}
+	}
+
+	if currentIv == nil {
+		currentIv, err = invoices.GetDraft(locId, user.Id, time.Now())
+		if err != nil {
+			return fmt.Errorf("get draft: %v", err)
+		}
+	}
+
+	inv := Invoice{Invoice: *currentIv}
+
+	if err = inv.load(*data); err != nil {
+		return fmt.Errorf("load: %v", err)
+	}
+
+	if err := inv.InvoiceUserMemberships(data); err != nil {
+		return fmt.Errorf("invoice user memberships: %v", err)
+	}
+
+	return
+}
+
 func taskData(locId int64) (
-	umsByUid map[int64]*user_memberships.UserMembership,
 	invsByUid map[int64][]*invoices.Invoice,
 	us []*users.User,
 	err error,
 ) {
-	ums, err := user_memberships.GetAllAt(locId)
-	if err != nil {
-		return nil, nil, nil, fmt.Errorf("get all user memberships: %v", err)
-	}
-
-	umsByUid = make(map[int64]*user_memberships.UserMembership)
-	for _, um := range ums {
-		umsByUid[um.UserId] = um
-	}
-
 	invs, err := invoices.GetAllInvoices(locId)
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("get all invoices: %v", err)
+		return nil, nil, fmt.Errorf("get all invoices: %v", err)
 	}
 
-	invsByUid = make(map[int64][]*Invoice)
+	invsByUid = make(map[int64][]*invoices.Invoice)
 	for _, inv := range invs {
 		if _, ok := invsByUid[inv.UserId]; !ok {
-			invsByUid[inv.UserId] = make([]*Invoice, 0, 10)
+			invsByUid[inv.UserId] = make([]*invoices.Invoice, 0, 10)
 		}
 		invsByUid[inv.UserId] = append(invsByUid[inv.UserId], inv)
 	}
 
 	us, err = users.GetAllUsersAt(locId)
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("get all users: %v", err)
+		return nil, nil, fmt.Errorf("get all users: %v", err)
 	}
 
 	return
