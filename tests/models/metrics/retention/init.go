e8d04e1b867878b2310fb498e92d7811ad83b544
Philip Silva <philip@fablab.berlin>
2016-11-28 11:23:08 +0100

    Fundament for retention

---
+++ tests/models/metrics/retention/init.go
@@ -0,0 +1,9 @@
+package retention
+
+import (
+	"github.com/FabLabBerlin/localmachines/tests/setup"
+)
+
+func init() {
+	setup.ConfigDir("/../../..")
+}
---
+++ tests/models/metrics/retention/retention_test.go
@@ -0,0 +1,43 @@
+package retention
+
+import (
+	"github.com/FabLabBerlin/localmachines/lib/day"
+	"github.com/FabLabBerlin/localmachines/models/invoices/invutil"
+	"github.com/FabLabBerlin/localmachines/models/metrics/retention"
+	"github.com/FabLabBerlin/localmachines/tests/setup"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func init() {
+	setup.ConfigDB()
+}
+
+func TestRetention(t *testing.T) {
+
+	Convey("Testing Retention", t, func() {
+		Reset(setup.ResetDB)
+
+		Convey("Calculate", func() {
+			Convey("Empty for no data", func() {
+				r := retention.New(
+					1,
+					7,
+					day.New(2016, 1, 1),
+					day.New(2016, 12, 31),
+					[]*invutil.Invoice{},
+				)
+				triangle := r.Calculate()
+				So(len(triangle), ShouldEqual, 53)
+				for i, row := range triangle {
+					So(len(row.Returns), ShouldEqual, 52-i)
+					So(row.StepDays, ShouldEqual, 7)
+					So(row.Users, ShouldEqual, 0)
+					for _, retrn := range row.Returns {
+						So(retrn, ShouldEqual, 0.0)
+					}
+				}
+			})
+		})
+	})
+}
