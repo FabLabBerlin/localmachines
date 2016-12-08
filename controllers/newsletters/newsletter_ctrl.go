16c7d150f03c4578005fe0068b9f165ad3dd8c61
Philip Silva <philip@fablab.berlin>
2016-12-08 15:45:34 +0100

    package for mailchimp

--- controllers/newsletters/newsletter_ctrl.go
+++ controllers/newsletters/newsletter_ctrl.go
@@ -2,26 +2,13 @@
 package newsletters
 
 import (
-	"bytes"
-	"encoding/json"
 	"github.com/FabLabBerlin/localmachines/controllers"
+	"github.com/FabLabBerlin/localmachines/lib/mailchimp"
 	"github.com/astaxie/beego"
-	"io"
-	"net/http"
-	"strings"
 )
 
 type Controller struct {
 	controllers.Controller
-}
-
-type SubscribeRequest struct {
-	ApiKey string `json:"apikey"`
-	Id     string `json:"id"`
-	Email  struct {
-		Email string `json:"email"`
-	} `json:"email"`
-	SendWelcome bool `json:"send_welcome"`
 }
 
 // @Title EasylabDev
@@ -31,45 +18,16 @@ type SubscribeRequest struct {
 // @Failure	500	Internal Server Error
 // @router /easylab_dev [post]
 func (c *Controller) EasylabDev() {
-	url := urlPrefix() + "/lists/subscribe.json"
-	req := SubscribeRequest{
+	req := mailchimp.Subscription{
 		ApiKey:      beego.AppConfig.String("mailchimpapikey"),
 		Id:          beego.AppConfig.String("mailchimpdevnewsletterlistid"),
 		SendWelcome: true,
 	}
 	req.Email.Email = c.GetString("email")
-	buf := bytes.Buffer{}
-	enc := json.NewEncoder(&buf)
-	if err := enc.Encode(req); err != nil {
-		beego.Error("Error marshalling request:", err)
-		c.Abort("500")
+	if err := req.Submit(); err != nil {
+		beego.Error("submit to easylab beta newsletter:", err)
+		c.Fail(500)
 	}
-	resp, err := http.Post(url, "application/json", &buf)
-	if err != nil {
-		beego.Error("Error subscribing E-Mail:", err)
-		c.Abort("500")
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		buf := bytes.Buffer{}
-		io.Copy(&buf, resp.Body)
-		if s := buf.String(); !strings.Contains(s, "List_AlreadySubscribed") {
-			beego.Error("Status code not 200:", buf.String())
-			c.Abort("500")
-		}
-	}
-	c.ServeJSON()
-}
 
-func urlPrefix() string {
-	apikey := beego.AppConfig.String("mailchimpapikey")
-	tmp := strings.Split(apikey, "-")
-	if len(tmp) != 2 {
-		beego.Error("mailchimpapikey has no part after the dash")
-		return ""
-	}
-	prefix := "https://"
-	prefix += tmp[1]
-	prefix += ".api.mailchimp.com/2.0/"
-	return prefix
+	c.ServeJSON()
 }
---
+++ lib/mailchimp/mailchimp.go
@@ -0,0 +1,57 @@
+package mailchimp
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/astaxie/beego"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type Subscription struct {
+	ApiKey string `json:"apikey"`
+	Id     string `json:"id"`
+	Email  struct {
+		Email string `json:"email"`
+	} `json:"email"`
+	SendWelcome bool `json:"send_welcome"`
+}
+
+func (req Subscription) Submit() (err error) {
+	url := urlPrefix() + "/lists/subscribe.json"
+
+	buf := bytes.Buffer{}
+	enc := json.NewEncoder(&buf)
+	if err := enc.Encode(req); err != nil {
+		return fmt.Errorf("Error marshalling request: %v", err)
+	}
+	resp, err := http.Post(url, "application/json", &buf)
+	if err != nil {
+		return fmt.Errorf("Error subscribing E-Mail: %v", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		buf := bytes.Buffer{}
+		io.Copy(&buf, resp.Body)
+		if s := buf.String(); !strings.Contains(s, "List_AlreadySubscribed") {
+			return fmt.Errorf("Status code not 200: %v", buf.String())
+		}
+	}
+
+	return
+}
+
+func urlPrefix() string {
+	apikey := beego.AppConfig.String("mailchimpapikey")
+	tmp := strings.Split(apikey, "-")
+	if len(tmp) != 2 {
+		beego.Error("mailchimpapikey has no part after the dash")
+		return ""
+	}
+	prefix := "https://"
+	prefix += tmp[1]
+	prefix += ".api.mailchimp.com/2.0/"
+	return prefix
+}
